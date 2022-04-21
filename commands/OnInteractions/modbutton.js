3f1c78514a1df688a94ab0a92538c09ef5404634
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-04-22 01:56:09 +0200

    updated mod button interaction to add missing commands

--- commands/OnInteractions/modbutton.js
+++ commands/OnInteractions/modbutton.js
@@ -4,6 +4,6 @@ type: "interaction",
 prototype: "button",
 code: `$title[1;**🛡 Mod**]
 $addField[1;Other Moderation Tools;\`serverinfo\`]
-$addField[1;Actions;\`kick\` \`purge\` \`setnick\`]
+$addField[1;Actions;\`kick\` \`purge\` \`setnick\` \`timeout\` \`untimeout\`]
 `
 }
