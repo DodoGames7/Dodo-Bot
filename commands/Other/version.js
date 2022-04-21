3dbaadfca4aa1aae1b934d5a7d79ca0093d474ef
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-04-22 01:56:55 +0200

    update bot version to 1.5.3

--- commands/Other/version.js
+++ commands/Other/version.js
@@ -3,10 +3,8 @@ module.exports ={
   aliases:["updates","news","ver"],
   code: `$title[1;** ⚙️Version: 1.5.3 (2022 Apr 14th)**]
 $description[1;
-**New Fun Commands**
-↪️ cat facts command (\`+cat-facts\`)
-↪️ dog facts command (\`+dog-facts\`) 
-↪️ token command (\`+token\`)
+**Changes**
+ ↪️ brought back timeout feature
 ]
 $footer[1;expect more to come Soon!]
 $color[1;#673ab7]`
