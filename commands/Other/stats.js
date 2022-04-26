6030843517e5d51f9ecd1c95a4ca7ddd4582fdbe
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-04-26 04:00:04 +0200

    reduce the unnecessary embed size

--- commands/Other/stats.js
+++ commands/Other/stats.js
@@ -1,12 +1,13 @@
 module.exports = {
 name: "stats",
 code: `$title[1;$username[$clientID] stats]
-$addField[1;Server Count;$serverCount]
-$addField[1;Aoi.js Version;$packageVersion]
-$addField[1;Uptime;$uptime]
-$addField[1;Ping;$pingms]
-$addField[1;Cpu Usage;$cpu]
-$addField[1;Ram Usage;$ram]
-$addField[1;User Count;$allMembersCount]
+$addField[1;Dependencies;$packageDependencies;no]
+$addField[1;Server Count;$serverCount;yes]
+$addField[1;Aoi.js;\`$packageVersion\`;yes]
+$addField[1;Uptime;$uptime;yes]
+$addField[1;Ping;$pingms;yes]
+$addField[1;Cpu Usage;$cpu;yes]
+$addField[1;Ram Usage;$ram;yes]
+$addField[1;User Count;$allMembersCount;yes]
 `
 } // gets stats's information
