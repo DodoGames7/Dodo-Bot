3b3f958bc6e04f30dda66e21e651d8437af27673
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-04-26 04:10:04 +0200

    Update version.js

--- commands/Other/version.js
+++ commands/Other/version.js
@@ -1,10 +1,10 @@
 module.exports ={
   name:"version",
   aliases:["updates","news","ver"],
-  code: `$title[1;** ⚙️Version: 1.5.4 (2022 Apr 18th)**]
+  code: `$title[1;** ⚙️Version: 1.5.5 (2022 Apr 26th)**]
 $description[1;
 **Changes**
- ↪️ brought back timeout feature
+ ↪️ reduced the unnecessary embed size of stats command
 ]
 $footer[1;expect more to come Soon!]
 $color[1;#673ab7]`
