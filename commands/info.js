32753e10cf8c6290e9cef3bcfaa1423f83816c34
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-05-08 19:06:15 +0200

    add buttons

--- commands/info.js
+++ commands/info.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+const {MessageEmbed, MessageActionRow, MessageButton} = require('discord.js');
 
 
 const { SlashCommandBuilder } = require('@discordjs/builders');
@@ -8,12 +8,32 @@ module.exports = {
   .setName('info')
   .setDescription('view important information'),
   async execute(interaction) {
-    const embedinfo = new Discord.MessageEmbed()
+    const embedinfo = new MessageEmbed()
     .setTitle('regarding discord.js mode')
-    .setDescription('this literally isnt currently finished, i will try to do my best with it, with some js knowledge, most likely the bot is running through [Glitch](https://glitch.com/edit/#!/dodo-bot-discord-bot)')
+    .setDescription('this literally isnt currently finished, i will try to do my best with it, with some js knowledge, if you have questions, want to check the source code then there are buttons shown below you can click')
     .setFooter('testing bot made by dodoGames')
     .setTimestamp();
-    return interaction.reply({ embeds: [embedinfo] });
+
+     const supportbutton = new MessageActionRow()
+    .addComponents(
+      new MessageButton()
+      .setURL('https://discord.gg/pFwKjAaZvj')
+      .setLabel('support')
+      .setStyle('LINK')
+      .setDisabled(false)
+     );
+
+    const sourcecodebutton = new MessageActionRow()
+    .addComponents(
+      new MessageButton()
+      .setURL('https://github.com/DodoGames7/Dodo-Bot/tree/djs-v13')
+      .setLabel('Source Code')
+      .setStyle('LINK')
+      .setDisabled(false),
+    );
+
+    
+    return interaction.reply({ embeds: [embedinfo], components: [supportbutton, sourcecodebutton]  });
     },
   
 };
