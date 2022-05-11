c1fc3224794762da06f5d9f7d38e68838aad63a8
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-05-11 08:43:09 +0200

    Update funbutton.js

--- commands/OnInteractions/funbutton.js
+++ commands/OnInteractions/funbutton.js
@@ -4,7 +4,7 @@ type: "interaction",
 prototype: "button",
 code: `$title[1;**🤣 Fun**]
 $addField[1;Other Fun Stuff;\`cat-facts\` \`dog-facts\` \`token\`]
-$addField[1;Mini Games;\`rps\`]
+$addField[1;Mini Games;\`rps\` \`snake\` \`connectfour\` \`trivia\` \`tictactoe\`]
 $addField[1;Images;\`eject\` \`jail\` \`comment\` \`wasted\` \`triggered\`]
 
 `
