e3035ddfd451452ca1b6c18f7449b5311ba35b9e
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-05-11 08:30:53 +0200

    Update version.js

--- commands/Other/version.js
+++ commands/Other/version.js
@@ -1,10 +1,18 @@
 module.exports ={
   name:"version",
   aliases:["updates","news","ver"],
-  code: `$title[1;** ⚙️Version: 1.5.5 (2022 Apr 26th)**]
+  code: `$title[1;** ⚙️Version: 1.5.6 (2022 may 11th)**]
 $description[1;
 **Changes**
- ↪️ reduced the unnecessary embed size of stats command
+ new mini games!
+↪️connectfour command
+↪️trivia command
+↪️tictactoe command
+↪️snake command
+changed rps game a little bit to allow to play with another user
+**Fixes**
+removed error codes for all mini games
+
 ]
 $footer[1;expect more to come Soon!]
 $color[1;#673ab7]`
