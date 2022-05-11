b7f707c100032e5b7b93546f23e3582f87121e78
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-05-11 08:38:43 +0200

    Create trivia.js

---
+++ commands/Fun/trivia.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: "trivia",
+  aliases:["triv"],
+  code: `$djsEval[const { Trivia } = require('discord-gamecord')
+new Trivia({
+  message: message,
+  slash_command: false,
+  embed: {
+    title: 'Trivia',
+    description: 'You have {time} seconds to respond!',
+    color: '#5865F2',
+  },
+  difficulty: 'medium',
+  winMessage: 'GG, Your answer was correct! It was **{answer}**',
+  loseMessage: 'Your answer was Incorrect! The correct answer was **{answer}**',
+  othersMessage: 'You are not allowed to use buttons for this message!',
+}).startGame();]`
+}
