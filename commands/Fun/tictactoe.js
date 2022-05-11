57b3c9a1b4de86af1b8ffdd17850f9dd8f7b8f74
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-05-11 08:35:37 +0200

    Create tictactoe.js

---
+++ commands/Fun/tictactoe.js
@@ -0,0 +1,29 @@
+module.exports = {
+  name: "tictactoe",
+  aliases:["ttt"],
+  code: `$djsEval[const { TicTacToe } = require('discord-gamecord')
+new TicTacToe({
+  message: message,
+  slash_command: false,
+  opponent: message.mentions.users.first(),
+  embed: {
+    title: 'Tic Tac Toe',
+    overTitle: 'Game Over',
+    color: '#5865F2',
+  },
+  oEmoji: '🔵',
+  xEmoji: '❌',
+  blankEmoji: '➖',
+  oColor: 'PRIMARY',
+  xColor: 'DANGER',
+  waitMessage: 'Waiting for the opponent...',
+  turnMessage: '{emoji} | Its now **{player}** turn!',
+  askMessage: 'Hey {opponent}, {challenger} challenged you for a game of Tic Tac Toe!',
+  cancelMessage: 'Looks like they refused to have a game of Tic Tac Toe. \:(',
+  timeEndMessage: 'Since the opponent didnt answer, i dropped the game!',
+  drawMessage: 'It was a draw!',
+  winMessage: '{emoji} | **{winner}** won the game!',
+  gameEndMessage: 'The game went unfinished :(',
+}).startGame();]
+$argsCheck[1;mention someone to play?]`
+}
