b41b515b99b414206a9ec09cc7bede37987de0e8
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-06-22 21:53:44 +0200

    Delete encode.js

--- commands/utils/encode.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "encode",
-  code: `$sendMessage[$getObjectProperty[binary];no]
-$createObject[$jsonRequest[https://api.popcat.xyz/encode?text=$toLowercase[$replaceText[$message; ;+;-1]]]
-$onlyif[$checkContains[$message;@everyone;@here;<@;<@&;<@!]==false;you cannot use mentions to encode text]
-$argsCheck[1;you need to provide me something to encode it]
-  `
-}
