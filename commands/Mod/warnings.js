0c2aa475d1c46e82c41a366e150cf06a57145578
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-06-22 21:52:21 +0200

    same reason

--- commands/Mod/warnings.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  name: "warnings",
-  code: `$title[1;Warnings for $username[$mentioned[1]]
-  $addField[1;Warnings Count;\`$getServerVar[warns;$mentioned[1]]\` 
-$onlyForIDS[$botOwnerID;]`
-}
