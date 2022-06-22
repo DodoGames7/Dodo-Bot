59e0c1a1942c7c8d8501cf309269178e55b814c3
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-06-22 21:11:47 +0200

    Delete triggered.js

--- commands/Fun/triggered.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-name: "triggered",
-code: `$attachment[https://some-random-api.ml/canvas/triggered?avatar=$replaceText[$userAvatar[$mentioned[1;yes]];.webp;.png;-1];triggered.png]`
-    } // makes users avatar Triggered
