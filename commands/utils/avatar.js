147adc54aa795cd2e33abe29a908ce6fc624b984
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-06-22 22:06:58 +0200

    fix alias

--- commands/utils/avatar.js
+++ commands/utils/avatar.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: "avatar",
-  aliases: ["ava","userav"],
+  aliases: ["av","userav"],
   code: `$title[1;$username[$mentioned[1;yes]]'s Avatar]
   $image[1;$userAvatar[$mentioned[1;yes]] `
 }
