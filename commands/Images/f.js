4fa76e54b97da32b1f91cb2aa0d92c99ea7a405b
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-07-07 00:16:36 +0200

    f cmd

---
+++ commands/Images/f.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: "respect",
+   aliases: ["f", "sadge"],
+  code: `$attachment[https://api.miduwu.ga/image/f?image=$replaceText[$userAvatar[$mentioned[1;yes]];.webp;.png;-1];f.png]`
+}
