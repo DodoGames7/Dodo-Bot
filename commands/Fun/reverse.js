ad2d494d8953226aecbd28a4ecf0a9e238274971
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-07-07 00:29:22 +0200

    same thing as yt cmd

--- commands/Fun/reverse.js
+++ commands/Fun/reverse.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "reverse",
   code: `$sendMessage[$getObjectProperty[text];no]
-$createObject[$jsonRequest[https://api.popcat.xyz/reverse?text=$toLowercase[$replaceText[$message; ;+;-1]]]
+$createObject[$jsonRequest[https://api.popcat.xyz/reverse?text=$uri[$message;encode]]]
 $onlyif[$checkContains[$message;@everyone;@here;<@;<@&;<@!]==false;you cannot use mentions to reverse text]
 $onlyIf[$message!=;you need to provide me something to reverse it]
   `
