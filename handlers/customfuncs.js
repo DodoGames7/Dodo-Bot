6dc4913b3c589bc0335f6b6660af75c7311b682e
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-07-06 02:14:23 +0200

    add $os func
    doesn't do much but useful if you want to list it as a additional info

--- handlers/customfuncs.js
+++ handlers/customfuncs.js
@@ -4,8 +4,22 @@ module.exports=(bot)=>{
 params : [],//THE TYPE OF PARAMS
 type : 'aoi.js', //TYPE METHOD
 code : ` 
-$attachment[$userAvatar[$botOwnerID];dodoGames.png]
+$attachment[$userAvatar[632607624742961153];dodoGames.png]
 ` //THE ACTUAL CODE IT WILL BE RETURN
-})
-  
+}) 
+  bot.functionManager.createCustomFunction({
+    name : '$os',
+    type : 'djs',
+    code : async d => {
+      const data = d.util.aoiFunc(d);
+      const os = require(`os`)
+      data.result = os.platform();
+      
+    return {
+        code: d.util.setCode(data)
+    }}
+
+   }) 
+  // returns the current operating system the host uses
+ 
 }
