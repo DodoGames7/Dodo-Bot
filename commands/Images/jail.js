f6a16b8ccd6fecf5760b6d6ae3f4288e817ceee7
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-07-14 20:45:58 +0200

    same reason

--- commands/Images/jail.js
+++ commands/Images/jail.js
@@ -1,5 +1,5 @@
 module.exports = {
 name: "jail",
 code: `$attachment[https://some-random-api.ml/canvas/jail?avatar=$replaceText[$userAvatar[$mentioned[1;yes]];.webp;.png;-1];jail.png]
-$onlyBotPerms[attachfiles;i need \`ATTACH_FILES\` Permissions before posting generating results]`
+$onlyBotPerms[attachfiles;i need \`ATTACH_FILES\` Permissions before posting generated results]`
     } // sends someone to jail
