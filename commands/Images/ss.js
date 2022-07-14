367c04d6800c4d4e4fc4ccd4ca63dc06cacc94d9
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-07-14 21:16:53 +0200

    i need to look for a better api for ss

--- commands/Images/ss.js
+++ commands/Images/ss.js
@@ -4,6 +4,6 @@ module.exports = {
   code: `$attachment[https://api.popcat.xyz/screenshot?url=$message;screenshot.png]
   $onlyIf[$isValidLink[$message[1]]==true;Hmm, not a valid URL]
   $argsCheck[1;at least provide a url]
-  $onlyBotPerms[attachfiles;i need \`ATTACH_FILES\` Permissions before posting generating results]
+  $onlyBotPerms[attachfiles;i need \`ATTACH_FILES\` Permissions before posting generated results]
   `
 }
