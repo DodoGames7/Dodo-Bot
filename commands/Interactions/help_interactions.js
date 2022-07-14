23de8449b8c7be14056f69c8e3ca8ca5e5b62f94
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-07-14 22:09:23 +0200

    update select menu interaction

--- commands/Interactions/help_interactions.js
+++ commands/Interactions/help_interactions.js
@@ -5,16 +5,16 @@ module.exports = {
   prototype: "selectMenu",
   code: `$if[$message==mod]
 $interactionUpdate[;{newEmbed:{title:**🛡 Mod**}{field:Actions:\`kick\` \`purge\` \`setnick\` \`timeout\` \`untimeout\`
-:no}{field:Other Moderation Tools:\`serverinfo\`:no};{actionRow:{selectMenu:help:choose another required:1:1:no:{selectMenuOptions:Mod:mod:Mod Commands category!:no:🛡}{selectMenuOptions:Fun:fun:Fun Commands category!:no:🤣}{selectMenuOptions:Utility:utility:Utility Commands category!:no:⚙️}}
+:no}{field:Other Moderation Tools:\`serverinfo\`:no};{actionRow:{selectMenu:help:Select an Category:1:1:no:{selectMenuOptions:Mod:mod:Mod Commands category!:no:🛡}{selectMenuOptions:Fun:fun:Fun Commands category!:no:🤣}{selectMenuOptions:Utility:utility:Utility Commands category!:no:⚙️}}
 ;;no}]
 $endif
 $if[$message==fun]
 $interactionUpdate[;{newEmbed:{title:**🤣 Fun**}{field:images:\`eject\` \`jail\` \`comment\` \`wasted\` \`triggered\` \`drip\` \`screenshot\` \`tweet\` \`emer\` \`gun\` \`drake\` \`delete\` \`f\` \`wanted\`
-:no}{field:Mini Games:\`rps\` \`snake\` \`connectfour\` \`trivia\` \`tictactoe\` \`akinator\` \`2048\` \`football\` \`flood\`:no}{field:text modifyers:\`reverse\` \`mock\` \`owoify\`:no}{field:Other Fun Stuff:\`fact-animals\` \`token\` \`8ball\`:no};{actionRow:{selectMenu:help:choose another required:1:1:no:{selectMenuOptions:Mod:mod:Mod Commands category!:no:🛡}{selectMenuOptions:Fun:fun:Fun Commands category!:no:🤣}{selectMenuOptions:Utility:utility:Utility Commands category!:no:⚙️}}
+:no}{field:Mini Games:\`rps\` \`snake\` \`connectfour\` \`trivia\` \`tictactoe\` \`akinator\` \`2048\` \`football\` \`flood\` \`fast-click\`:no}{field:text modifyers:\`reverse\` \`mock\` \`owoify\`:no}{field:Other Fun Stuff:\`fact-animals\` \`token\` \`8ball\` \`randomtopic\`:no};{actionRow:{selectMenu:help:Select an Category:1:1:no:{selectMenuOptions:Mod:mod:Mod Commands category!:no:🛡}{selectMenuOptions:Fun:fun:Fun Commands category!:no:🤣}{selectMenuOptions:Utility:utility:Utility Commands category!:no:⚙️}}
 ;;no]
 $endif
 $if[$message==utility]
-$interactionUpdate[;{newEmbed:{title:**⚙️ Utilty**}{field:Misc:\`fact\` \`av\`:no}{field:getting information:\`npm\` \`github\` \`youtube\` \`mcserver\` \`weather\`:no}{field:customization:\`set-prefix\` \`reset-prefix\` \`akinator-options\` \`set-trivia-mode\`:no}{field:bot itself:\`stats\` \`info\` \`ver\` \`prefix\` \`support\`:no};{actionRow:{selectMenu:help:choose another required:1:1:no:{selectMenuOptions:Mod:mod:Mod Commands category!:no:🛡}{selectMenuOptions:Fun:fun:Fun Commands category!:no:🤣}{selectMenuOptions:Utility:utility:Utility Commands category!:no:⚙️}}
+$interactionUpdate[;{newEmbed:{title:**⚙️ Utilty**}{field:Misc:\`fact\` \`av\`:no}{field:getting information:\`npm\` \`github\` \`youtube\` \`mcserver\` \`weather\` \`djs\`:no}{field:customization:\`set-prefix\` \`reset-prefix\` \`akinator-options\` \`set-trivia-mode\`:no}{field:bot itself:\`stats\` \`info\` \`ver\` \`prefix\` \`support\` \`credits\`:no};{actionRow:{selectMenu:help:Select an Category:1:1:no:{selectMenuOptions:Mod:mod:Mod Commands category!:no:🛡}{selectMenuOptions:Fun:fun:Fun Commands category!:no:🤣}{selectMenuOptions:Utility:utility:Utility Commands category!:no:⚙️}}
 ;;no}]
 $endif`
 }
