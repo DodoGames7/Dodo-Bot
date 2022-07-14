760a65dd14b7a25ad7f4e689feb367fa261c5202
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-07-14 21:10:02 +0200

    setup responses for things such as logs

---
+++ commands/responses/deletedlog.js
@@ -0,0 +1,13 @@
+module.exports = {
+  channel: "$getServerVar[deletedlogs]",
+   type: "messageDelete",
+  name: "deletelog",
+  code: `$author[1;$userTag[$authorID];$authorAvatar]
+  $title[1;a message deleted!]
+  $description[1; message $message of $username[$authorID] was deleted in $channelName[$channelused]]
+  $addTimeStamp[1]
+  $color[1;RED]
+  $onlyIf[$getServerVar[deletedlogs]!=none;]
+`
+
+}
