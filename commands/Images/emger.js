e34375307aafbcb07fdc2979057da3ad49cf3282
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-07-14 21:16:19 +0200

    Update emger.js

--- commands/Images/emger.js
+++ commands/Images/emger.js
@@ -3,6 +3,6 @@ module.exports = {
   aliases: ["emger", "emergency"],
   code: `$attachment[https://vacefron.nl/api/emergencymeeting?text=$message;emergency.png]
   $onlyIf[$message!=;the players would probably have ejected you already for being doing it for no reason]
-  $onlyBotPerms[attachfiles;i need \`ATTACH_FILES\` Permissions before posting generating results]
+  $onlyBotPerms[attachfiles;i need \`ATTACH_FILES\` Permissions before posting generated results]
   `
 }
