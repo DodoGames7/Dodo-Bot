664d4db943e6939b322872ca2aef653919b62e54
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-07-24 22:47:56 +0200

    remove beta tag in akin button

--- commands/utils/akin-options.js
+++ commands/utils/akin-options.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "akinator-options",
   code: `$title[1;Akinator Options]
   $description[1;you can customize options of akinator game, use buttons below to select what do you want to change]
-  $addButton[1;Language (Beta);primary;akinlanguage;no]
+  $addButton[1;Language;primary;akinlanguage;no]
   $addButton[1;Game Mode;primary;akingamemode;no]
   $addButton[1;Use Buttons;primary;useakinbuttons;no]
 $editIn[5m; {
