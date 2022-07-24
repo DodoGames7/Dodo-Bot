9c29140f0f3e16de4a4561df79ec0a119b3160b8
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-07-24 23:16:32 +0200

    Update help.js

--- commands/Other/help.js
+++ commands/Other/help.js
@@ -1,14 +1,13 @@
 module.exports ={
+  type: "basicCommand",
   name:"help",
   code: `$title[1;Help Commands]
-$description[1; select one of the buttons to check Commands!]
-$addButton[1;Utility;secondary;utility;no;⚙️]
-$addButton[1;Fun;secondary;fun;no;🤣]
-$addButton[1;Mod;secondary;mod;no;🛡]
-$footer[1;Rewrite version maintained by dodoGames]
-$color[1;#673ab7]
-$editIn[10m; {
- "embeds" : "{newEmbed:{title:Help Commands}{description:select one of the buttons to check Commands!}}" ,
- "components" : "{actionRow:{button:Mod:secondary:mod:yes:🛡}{button:Fun:secondary:fun:yes:🤣}{button:Utility:secondary:utility:yes:⚙️}}"
- }]`
+$description[1; under construction]
+$addActionRow
+$addSelectMenu[help;Select an Category!;1;1;false]
+$addSelectMenuOption[Mod;Mod Commands Category!;mod;🛡;false]
+$addSelectMenuOption[Fun;Fun Commands Category!;fun;🤣;false]
+$footer[1;v2 maintained by dodoGames]
+$color[1;Blurple]
+`
 } 
