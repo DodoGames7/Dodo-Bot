6bb1bf44dafec4ab218149643a8ad4e55f1d5d1b
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-07-24 23:02:11 +0200

    why i forget this

--- commands/Other/version.js
+++ commands/Other/version.js
@@ -5,7 +5,7 @@ module.exports ={
   code: `$title[1;** ⚙️Version: 2.0.0-next (2022 July 24th)**]
 $description[1;
 **MAJOR CHANGES**
-drop for aoi.js v5 and introduce support for aoi.js v6!
+drop support for aoi.js v5 and introduce support for aoi.js v6!
 use discord api v10, to avoid losing access for perfix cmds, \`MessageContent\` intent has been granted to avoid issues
 
 ]
