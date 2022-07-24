ea3af9a615a98f71e15adcdf93eba4d4fc547f14
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-07-24 22:45:24 +0200

    fix weather command being missing

---
+++ commands/utils/weather.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: "weather",
+  code: `$title[1;$getObjectProperty[location.name]]
+  $addField[1;windspeed;$getObjectProperty[current.windspeed]]
+  $addField[1;Time;$getObjectProperty[current.observationtime];yes]
+  $addField[1;temperature;$getObjectProperty[current.temperature]C;yes]
+  $addField[1;humidity;$getObjectProperty[current.humidity];yes]
+  $thumbnail[1;$getobjectProperty[current.imageUrl]]
+  $onlyIf[$getObjectProperty[location.name]!=undefined;cannot find results for what you searched for]
+  $createObject[$jsonRequest[https://apiv1.spapi.ga/fun/weather?place=$uri[$message;encode]`
+}
