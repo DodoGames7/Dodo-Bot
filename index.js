9ce0964e78f5355bd92633f66b700237f77d4ff2
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-07-24 22:53:02 +0200

    👀

--- index.js
+++ index.js
@@ -1,30 +1,13 @@
-const aoijs = require('aoi.js')
+const { AoiClient } = require("aoi.js");
 
-
-const bot = new aoijs.AoiClient({
-   token: process.env.TOKEN,
- //Discord Bot Token, (ofc it's hidden what did you expect)
-   prefix: ["$getServerVar[prefix]", "<@$clientID>"],  //Discord Bot Prefix
-   intents: "all"
- })
-
-
-
-bot.variables({
-prefix: "+",
-trivia_game_mode: "medium",
-akin_buttons: "false",
-akin_gametype: "character",
+const bot = new AoiClient({
+    token: process.env.TOKEN,
+    intents: ["Guilds", "GuildMessages", "MessageContent"],
+    prefix: "&"
 })
 
-bot.onInteractionCreate() // allows making interactions command work
+bot.addEvent("onMessage")
 
- bot.onMessage() //Allows to execute Commands
+bot.commands.load("./commands/")
 
- const loader = new aoijs.LoadCommands(bot)
- loader.load(bot.cmd,"./commands/")
-
- /*
- bot.cmd is object of Collections where the command data will be stored
- "./commands/" is the path of folder where all the commands' code will be present
- */
+bot.start()
