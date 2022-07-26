52c775a916c0ab6f81435bb3c7f8a3f428368254
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-07-26 20:13:16 +0200

    show id of deleted msg author

--- commands/responses/deletedlog.js
+++ commands/responses/deletedlog.js
@@ -5,6 +5,7 @@ module.exports = {
   code: `$author[1;$userTag[$authorID];$authorAvatar]
   $title[1;a message deleted!]
   $description[1; message $message of $username[$authorID] was deleted in $channelName[$channelused]]
+  $footer[1;ID: $authorID]
   $addTimeStamp[1]
   $color[1;RED]
   $onlyIf[$getServerVar[deletedlogs]!=none;]
