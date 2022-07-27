d3ccc522aeaf42fe62469421de05aadcc7e06f4c
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-07-27 15:38:09 +0200

    Update version.js

--- commands/Other/version.js
+++ commands/Other/version.js
@@ -1,11 +1,11 @@
 module.exports ={
   name:"version",
   aliases:["updates","news","ver"],
-  code: `$title[1;** ⚙️Version: 1.5.9 (2022 july 26th)** **(Beta)**]
+  code: `$title[1;** ⚙️Version: 1.5.9 (2022 july 26th)**]
 $description[1;
 **INTRODUCING LOGS**
 Logs are an useful moderation tool to get info about server activity, currently there is only one available feature which is deleted logs, a way to post logs of deleted messages, this is helpful especially if you're a moderator of a server
-to start, simply run \`&set-delete <#channel>\`, it's recommended to be private and the bot must have \`SEND_MESSAGES\` perm there!
+to start, simply run \`$getServerVar[prefix]set-delete <#channel>\`, it's recommended to be private and the bot must have \`SEND_MESSAGES\` perm there!
 
 **changes**
 added 4 new mini games such as \`flood\`, \`football\`, \`fast-click\` and lastly \`wordle\`
@@ -14,6 +14,7 @@ use select menu instead of buttons in help commands
 akinator language option has been officiaily enabled
 add \`djs\` and \`mcserver\` command
 added more image commands such as \`f\`, \`delete\` and \`drake\`
+added randomtopic command \`randomtopic\`
 
 **fixes**
 fixed grammar of utility name of utility menu
