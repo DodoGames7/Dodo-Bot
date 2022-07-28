84168b8bcbe2861f284b4e7bfab38e05f7fef3d5
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-07-28 21:06:08 +0200

    v2 unstable warnings

--- commands/Other/info.js
+++ commands/Other/info.js
@@ -1,9 +1,7 @@
 module.exports ={
-  name:"info",
-  code: `$title[1;about the Rewrite Version]
-$description[1;this rewrite version is coded in Aoi.js and it's currently in Development, this rewrite aims to be alternative to BDFD and it's restrictions as well as this allows me to make unlimited commands and make the bot really 24 hours online without having to renew hosting time in BDFD, but it's also open source this time and anyone can self host it with their very own custom bot, stay tuned for Updates! ❤️]
-$footer[1;Rewrite maintained by dodoGames!]
-$addButton[1;invite BDFD Version;link;https://discord.com/api/oauth2/authorize?client_id=748517727932776479&permissions=1100787084359&scope=bot%20applications.commands;no]
-$addButton[1;invite Rewrite Version;link;https://discord.com/api/oauth2/authorize?client_id=958351401719844874&permissions=1100787076167&scope=bot;no]
-$addButton[1;Source Code;link;https://github.com/DodoGames7/Dodo-Bot;no]`
-} // shows a important information regarding the Rewrite Version
+  type: "basicCommand",
+  name: "info",
+  code: `$title[1;about the v2 version]
+  $description[1;be careful!, v2 is an major update of dodo bot that's still work in progress, the support for this dev build isn't properly given, however you can still report bugs about v2 through github issues, lastly v2 features and updates are subjected to change at anytime at the final release of the update]
+  $footer[1;v2 Update maintained by dodoGames]`
+} // shows a important information regarding V2 Update
