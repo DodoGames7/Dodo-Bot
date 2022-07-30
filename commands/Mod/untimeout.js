cdd3dc3b01bf7fd981056a928d8de9914a7f45b2
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-07-30 21:46:24 +0200

    totally trying to make v1.6-beta bug killer

--- commands/Mod/untimeout.js
+++ commands/Mod/untimeout.js
@@ -1,8 +1,8 @@
 module.exports ={
     name: "untimeout",
-    code: `$timeoutMember[$guildID;$mentioned[1;yes];0s;no;$message[2];]
-successfully untimeouted $username[$mentioned[1;yes]] with Reason: $message[2]
-$argsCheck[>1;you need to provide a user to untimeout with a Reason]
-$onlyPerms[moderatemembers;you need \`MODERATE_MEMBERS\` Permission for that]
-$onlyBotPerms[moderatemembers;i need \`MODERATE_MEMBERS\` to do that!, how would i perform such a action?]`
+    code: `$timeoutMember[$guildID;$findMember[$message[1];yes];0s;no;$if[$messageslice[1]==;No reason provided;$messageslice[1]];]
+successfully untimeouted $username[$findMember[$message[1];yes]] with Reason: $if[$messageslice[1]==;No reason provided;$messageslice[1]]
+$argsCheck[>0;you need to provide a user to untimeout with a Reason]
+$onlyIf[$hasPerms[$guildid;$authorid;moderatemembers]==true;**$username** You are missing **\`MODERATE_MEMBERS\`** permission]
+$onlyIf[$hasPerms[$guildid;$clientid;moderatemembers]==true;**$username** I am missing **\`MODERATE_MEMBERS\`** permission]`
 }
