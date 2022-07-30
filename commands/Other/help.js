dcc296964255350198f1056b5548dc382321e528
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-07-30 23:49:34 +0200

    Update help.js

--- commands/Other/help.js
+++ commands/Other/help.js
@@ -2,7 +2,7 @@ module.exports ={
   name:"help",
   code: `$title[1;Help Commands]
 $description[1; select one of the options to check Commands!]
-$addSelectMenu[1;help;select an required option;1;1;no;Mod:Mod Commands category!:mod:no:🛡️;Fun:Fun Commands category!:fun:no:🤣;Utility:Utility Commands category!:utility:no:⚙️;Economy;Economy Commands category!:eco:no:🤑]
+$addSelectMenu[1;help;select an required option;1;1;no;Mod:Mod Commands category!:mod:no:🛡️;Fun:Fun Commands category!:fun:no:🤣;Utility:Utility Commands category!:utility:no:⚙️;Economy:Economy Commands category!:eco:no:🤑]
 $footer[1;Beta version maintained by dodoGames]
 $color[1;BLURPLE]
 $editIn[10m; {
