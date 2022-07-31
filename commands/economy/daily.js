1a44edab9680d46d884aaa94383d2d8b33179840
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-07-31 22:15:55 +0200

    grammar fix to daily cmd

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: "daily",
-  code: `$title[you have recicived 100$]
+  code: `$title[you have received 100$]
   $description[everyday you can claim your daily reward, so you can get richer!, to get reminded when to claim daily rewards, press button below]
   $addButton[1;Remind me;primary;daily;no;🎁]
   $setGlobalUserVar[money;$sum[$getGlobalUserVar[money];100]]
