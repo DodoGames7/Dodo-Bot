e63c244c31c74a1597319085a0d3264b25dc13f0
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-07-31 22:18:42 +0200

    update economy reminders interactions

--- commands/Interactions/economy_interactions.js
+++ commands/Interactions/economy_interactions.js
@@ -5,4 +5,11 @@ prototype: "button",
 code: `$interactionReply[you will be reminded after one day of daily!;;;;everyone;yes]
 $setTimeout[remind;1d;user: $interactionData[author.id]] 
 `
+},{
+  name: "weekly",
+  type: "interaction",
+  prototype: "button",
+  code: `$interactionReply[you will be reminded after one week!;;;;everyone;yes]
+$setTimeout[wremind;7d;user: $interactionData[author.id]]
+`
 }
