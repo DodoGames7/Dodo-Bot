32df20dbf1e917b7b3bc8cc61d9e1e906e25293f
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-07-31 22:20:31 +0200

    same thing

--- commands/responses/daily_remind.js
+++ commands/responses/daily_remind.js
@@ -7,4 +7,12 @@ module.exports = {
   $color[BLURPLE]
   $dm[$timeoutData[user]]
 `
+},{
+  name: "wremind",
+  type: "timeout",
+  code: `$title[you can now claim your weekly reward]
+  $description[it's been an week ago since you claimed your weekly reward!]
+  $color[BLURPLE]
+  $dm[$timeoutData[user]]
+  `
 }
