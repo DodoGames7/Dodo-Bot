6b6648cba64406c9746192cda79290533c93cdf0
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-08-02 23:19:51 +0200

    no v1.6??, get v1.7-beta then

--- commands/Other/version.js
+++ commands/Other/version.js
@@ -1,7 +1,7 @@
 module.exports ={
   name:"version",
   aliases:["updates","news","ver"],
-  code: `$title[1;** ⚙️Version: 1.6.0 (2022 july 28th)** **(Beta)**]
+  code: `$title[1;** ⚙️Version: 1.7.0 (2022 aug 2th)** **(Beta)**]
 $description[1;
 TO DO
 ]
