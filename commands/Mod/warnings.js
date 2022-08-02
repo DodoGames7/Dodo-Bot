25f961bbabdfca49ad80028d807159a344528562
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-08-02 23:45:36 +0200

    add warns cmd

---
+++ commands/Mod/warnings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: "warnings",
+  aliases: ["warns", "ws"],
+ `$title[$username[$mentioned[1]'s Warnings]
+ $addField[Warnings Count;\`$getUserVar[warns;$mentioned[1;yes];$guildID]\`;no]
+ $color[RED]
+  `
+  }
