a97a1bfd546b003df2dee6646618b11ef8b1c1c3
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-08-04 19:34:15 +0200

    last day of being v5 user😕

--- commands/Other/credits.js
+++ commands/Other/credits.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "credits",
   code: `$title[Dodo Bot Credits]
-  $description[dodoGames#7509 - main developer of the bot
+  $description[$userTag[632607624742961153] - main developer of the bot
   
   $userTag[760580796695969844] - for some code fixes and advices
   ]
