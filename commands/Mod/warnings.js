2e06f25476c9efd4e93d0dc118b573aff2b5813a
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-08-07 11:11:46 +0200

    fix warnings cmd being failing to load

--- commands/Mod/warnings.js
+++ commands/Mod/warnings.js
@@ -1,7 +1,9 @@
 module.exports = {
   name: "warnings",
   aliases: ["warns", "ws"],
- `$title[$username[$mentioned[1]'s Warnings]
+  code: 
+ `
+ $title[$username[$mentioned[1]'s Warnings]
  $addField[Warnings Count;\`$getUserVar[warns;$mentioned[1;yes];$guildID]\`;no]
  $color[RED]
   `
