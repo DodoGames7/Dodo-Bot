7b43d12016c014d7b9347f3db67e250ee398d451
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-08-18 21:00:18 +0200

    fix emojify cmd being missing

--- commands/Interactions/help_interactions.js
+++ commands/Interactions/help_interactions.js
@@ -10,7 +10,7 @@ $interactionUpdate[;{newEmbed:{title:**🛡 Mod**}{field:Actions:\`kick\` \`purg
 $endif
 $if[$message==fun]
 $interactionUpdate[;{newEmbed:{title:**🤣 Fun**}{field:images:\`eject\` \`jail\` \`comment\` \`wasted\` \`changemymind\` \`triggered\` \`drip\` \`screenshot\` \`tweet\` \`emer\` \`gun\` \`drake\` \`delete\` \`f\` \`wanted\`
-:no}{field:Mini Games:\`rps\` \`snake\` \`connectfour\` \`trivia\` \`tictactoe\` \`akinator\` \`2048\` \`football\` \`flood\` \`fast-click\` \`wordle\`:no}{field:text modifyers:\`reverse\` \`mock\` \`owoify\`:no}{field:Other Fun Stuff:\`fact-animals\` \`token\` \`8ball\` \`randomtopic\`:no};{actionRow:{selectMenu:help:Select an Category:1:1:no:{selectMenuOptions:Mod:mod:Mod Commands category!:no:🛡}{selectMenuOptions:Fun:fun:Fun Commands category!:no:🤣}{selectMenuOptions:Utility:utility:Utility Commands category!:no:⚙️}}
+:no}{field:Mini Games:\`rps\` \`snake\` \`connectfour\` \`trivia\` \`tictactoe\` \`akinator\` \`2048\` \`football\` \`flood\` \`fast-click\` \`wordle\`:no}{field:text modifyers:\`reverse\` \`mock\` \`owoify\` \`emojify\`:no}{field:Other Fun Stuff:\`fact-animals\` \`token\` \`8ball\` \`randomtopic\`:no};{actionRow:{selectMenu:help:Select an Category:1:1:no:{selectMenuOptions:Mod:mod:Mod Commands category!:no:🛡}{selectMenuOptions:Fun:fun:Fun Commands category!:no:🤣}{selectMenuOptions:Utility:utility:Utility Commands category!:no:⚙️}}
 ;;no]
 $endif
 $if[$message==utility]
