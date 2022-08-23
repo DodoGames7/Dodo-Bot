b89b92010344415a74b703bc941f4144dd214798
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-08-23 18:18:09 +0200

    enchancement to variables handler

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const bot = new aoijs.AoiClient({
 
 
 // handlers
-require('./handlers/variables')(bot) // for bot variables (important, do not delete)
+bot.variables(require("./handlers/variables.js")); // for bot variables (important, do not delete)
 require('./handlers/callbacks')(bot) // for loading most callbacks used in bot 
 require('./handlers/customfuncs')(bot) // for loading custom made functions using function manager
 
