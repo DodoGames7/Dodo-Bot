b6e2d8551a78ea0e4d10e80ed992d126a9ea2d0c
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-08-25 17:07:09 +0200

    prepare v2-dev for v6-dev on 26th

--- commands/Other/help.js
+++ commands/Other/help.js
@@ -7,6 +7,7 @@ $addActionRow
 $addSelectMenu[help;Select an Category!;1;1;false]
 $addSelectMenuOption[Mod;Mod Commands Category!;mod;🛡;false]
 $addSelectMenuOption[Fun;Fun Commands Category!;fun;🤣;false]
+$addSelectMenuOption[config;configure on how the bot should work!;config;⚠;false]
 $footer[1;v2 maintained by dodoGames]
 $color[1;Blurple]
 `
