086a503e994d1ea503f40e9be16c15c8f2cb5057
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-08-25 17:11:32 +0200

    refresh changelog

--- commands/Other/version.js
+++ commands/Other/version.js
@@ -2,11 +2,13 @@ module.exports ={
   type: "basicCommand",
   name:"version",
   aliases:["updates","news","ver"],
-  code: `$title[1;** ⚙️Version: 2.0.0-next (2022 July 24th)**]
+  code: `$title[1;** ⚙️Version: 2.0.0-next (2022 aug 25th)**]
 $description[1;
 **MAJOR CHANGES**
 drop support for aoi.js v5 and introduce support for aoi.js v6!
 use discord api v10, to avoid losing access for perfix cmds, \`MessageContent\` intent has been granted to avoid issues
+change some stuff
+added new category config which is used to configure on how the bot should work
 
 ]
 $color[1;Blurple]`
