765ba0ab6a84567c0dea554dd0aa49ae4b63963c
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-08-26 01:52:14 +0200

    Update version.js

--- commands/Other/version.js
+++ commands/Other/version.js
@@ -1,13 +1,14 @@
 module.exports ={
   name:"version",
   aliases:["updates","news","ver"],
-  code: `$title[1;** ⚙️Version: 1.5.9-patched (2022 Aug 18th)**]
+  code: `$title[1;** ⚙️Version: 1.5.9-patched (2022 Aug 26th)**]
 $description[1;
 *NOTE: this release is an patch to fix certain stuff of dodo bot to bring stability*
 
 **bug fixes**
 fixed weather command being returning failed result if user didn't even specify anything such as city
 fixed emojify command being missing of help fun menu cmd
+fix non-existing moderation cmds being listed for some reason
 ]
 $color[1;BLURPLE]`
 } // returns a list of changes in the bot code
