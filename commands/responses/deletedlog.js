5ad8fa204903aeb615dec5df8884efb746aa799f
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-08-28 17:43:57 +0200

    log deleted msg change + $os -> $system

--- commands/responses/deletedlog.js
+++ commands/responses/deletedlog.js
@@ -4,7 +4,8 @@ module.exports = {
   name: "deletelog",
   code: `$author[1;$userTag[$authorID];$authorAvatar]
   $title[1;a message deleted!]
-  $description[1; message $message of $username[$authorID] was deleted in $channelName[$channelused]]
+  $description[1; message from $username[$authorID] was deleted in $channelName[$channelused]
+  **message**: $message]
   $footer[1;ID: $authorID]
   $addTimeStamp[1]
   $color[1;RED]
