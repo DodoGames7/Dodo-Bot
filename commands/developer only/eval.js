0f7fabe3a24fc2923a9c5759ac8fc74c9343eb81
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-09-03 00:28:24 +0200

    Update eval.js

--- commands/developer only/eval.js
+++ commands/developer only/eval.js
@@ -1,6 +1,14 @@
-module.exports ={
-name:"eval",
-code: `
-$eval[$message]
-$onlyForIDs[$botOwnerID;only $username[$BotOwnerID] can execute this command]
-`} // allows excuting a code for testing purposes
+module.exports = {
+  type: "basicCommand",
+  name: "eval",
+  code: `$onlyIf[$authorID==$clientOwnerID;you're not dev]
+  $onlyIf[$message!=;you need to type something to eval]
+  $let[input;$message]
+  $let[output;$eval[true;$message]]
+  $title[1;Aoi.js Eval]
+ $addField[1;input;\`\`\`$get[input]\`\`\`;false]
+ $addField[1;output;\`\`\`$get[output]\`\`\`;false]
+ $color[1;Green]
+  
+`
+}
