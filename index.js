94909115953e20e60e0035c17d6d1b7aae294c46
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-09-02 17:36:55 +0200

    v6 panel

--- index.js
+++ index.js
@@ -11,3 +11,15 @@ require('./utils/callbacks')(bot)
 bot.commands.load("./commands/")
 
 bot.start()
+
+// v6 panel dashboard
+const panel = new Panel({
+    username: process.env.username,//username for logging in
+    password: process.env.password,//password for logging in
+    secret: process.env.secret,//session secret
+    port: 3000,//port on which website is hosted, Not required! Default 3000
+    bot: bot,//your aoi.js client
+    mainFile: "index.js",//Main file where code is running.Not required, default taken from package.json
+    commands: "./commands",// folder name in which all the edit needing files are there.
+    vesrion:"v6"
+})
