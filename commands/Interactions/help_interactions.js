1b188cf6604bca0f2958407e2472f820b6ad141a
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-09-15 17:45:29 +0200

    help cmd updates

--- commands/Interactions/help_interactions.js
+++ commands/Interactions/help_interactions.js
@@ -5,7 +5,7 @@ module.exports = {
   prototype: "selectMenu",
   code: `$if[$message==mod]
 $interactionUpdate[;{newEmbed:{title:**🛡 Mod**}{field:Actions: \`ban\` \`kick\` \`purge\` \`setnick\` \`timeout\` \`untimeout\`
-:no}{field:Other Moderation Tools:\`serverinfo\` \`set-delete\` \`reset-delete\` \`warn\` \`unwarn\`:no};{actionRow:{selectMenu:help:Select an Category:1:1:no:{selectMenuOptions:Mod:mod:Mod Commands category!:no:🛡}{selectMenuOptions:Fun:fun:Fun Commands category!:no:🤣}{selectMenuOptions:Utility:utility:Utility Commands category!:no:⚙️}{selectMenuOptions:Economy:eco:Economy Commands category!:no:🤑}}
+:no}{field:Other Moderation Tools:\`serverinfo\` \`set-delete\` \`reset-delete\` \`warn\` \`unwarn\` \`poll\`:no};{actionRow:{selectMenu:help:Select an Category:1:1:no:{selectMenuOptions:Mod:mod:Mod Commands category!:no:🛡}{selectMenuOptions:Fun:fun:Fun Commands category!:no:🤣}{selectMenuOptions:Utility:utility:Utility Commands category!:no:⚙️}{selectMenuOptions:Economy:eco:Economy Commands category!:no:🤑}}
 ;;no}]
 $endif
 $if[$message==fun]
@@ -14,7 +14,7 @@ $interactionUpdate[;{newEmbed:{title:**🤣 Fun**}{field:images:\`eject\` \`jail
 ;;no]
 $endif
 $if[$message==utility]
-$interactionUpdate[;{newEmbed:{title:**⚙️ Utility**}{field:Misc:\`fact\` \`av\` \`calu\`:no}{field:getting information:\`npm\` \`github\` \`youtube\` \`mcserver\` \`weather\` \`djs\` \`aoi\`:no}{field:customization:\`set-prefix\` \`reset-prefix\` \`akinator-options\` \`set-trivia-mode\`:no}{field:bot itself:\`stats\` \`info\` \`ver\` \`prefix\` \`support\` \`credits\` \`reports\`:no};{actionRow:{selectMenu:help:Select an Category:1:1:no:{selectMenuOptions:Mod:mod:Mod Commands category!:no:🛡}{selectMenuOptions:Fun:fun:Fun Commands category!:no:🤣}{selectMenuOptions:Utility:utility:Utility Commands category!:no:⚙️}{selectMenuOptions:Economy:eco:Economy Commands category!:no:🤑}}
+$interactionUpdate[;{newEmbed:{title:**⚙️ Utility**}{field:Misc:\`fact\` \`av\` \`calu\`:no}{field:getting information:\`npm\` \`github\` \`youtube\` \`mcserver\` \`weather\` \`djs\` \`aoi\`:no}{field:customization:\`set-prefix\` \`reset-prefix\` \`akinator-options\` \`set-trivia-mode\` \`poll set\`:no}{field:bot itself:\`stats\` \`info\` \`ver\` \`prefix\` \`support\` \`credits\` \`reports\`:no};{actionRow:{selectMenu:help:Select an Category:1:1:no:{selectMenuOptions:Mod:mod:Mod Commands category!:no:🛡}{selectMenuOptions:Fun:fun:Fun Commands category!:no:🤣}{selectMenuOptions:Utility:utility:Utility Commands category!:no:⚙️}{selectMenuOptions:Economy:eco:Economy Commands category!:no:🤑}}
 ;;no}]
 $endif
 $if[$message==eco]
