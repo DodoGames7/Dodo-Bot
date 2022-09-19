b53dbb23f2f44647ea5570201fb05067ce2d9b3f
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-09-19 20:39:00 +0200

    Update version.js

--- commands/Other/version.js
+++ commands/Other/version.js
@@ -1,7 +1,7 @@
 module.exports ={
   name:"version",
   aliases:["updates","news","ver"],
-  code: `$title[1;** ⚙️Version: 1.7.0 (2022 sep 14th)** **(Final Beta)**]
+  code: `$title[1;** ⚙️Version: 1.7.0 (2022 sep 19th)** **(Preview)**]
 $description[1;
   note: this version includes a lot of changes after two months of developing
 
@@ -11,16 +11,19 @@ added economy
 added chatbot system + poll system
 added copyright icon to \`credits\`
 added slash commands support (devs can choose to whether enable them or not now)
-allow embeds to be used in \`say\` command (`\$getServerVar[prefix]say embedded content\`)
+allow embeds to be used in \`say\` command (\`$getServerVar[prefix]say embedded content\`)
 
 **fixes**
 fixed a lot of bugs like mini games command arguments
 fixed \`info\` being outdated since rewrite starting development
 fixed errors appearing even if dev hasn't even specifed anything in dev cmds
 fixed \`credit\` cmd being outdated (especially one of the authors names)
+fixed automatic cooldown of disabling select menu after certain of time refusing to work and just weirdly removes the default blurple color
+fixed the wrong placeholder name for select menu of \`help\` command
 
 **other**
 added \`reports\` cmd
+bot now can managed by panel for devs
 ]
 $color[1;BLURPLE]`
 } // returns a list of changes in the bot code
