2780d85fec535d8354777889e8da2e3dd6dd86c5
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-10-14 11:30:45 +0200

    fix that

--- commands/economy/weekly.js
+++ commands/economy/weekly.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "weekly",
   code: `$title[you have received 300$]
   $description[every week you can claim your reward, so you can get richer!, to get reminded when to claim weekly rewards, press button below]
-  $addButton[1;Remind me;primary;weekly;no;🎁]
+  $addButton[1;Remind me;primary;weekly;no;⏲]
   $setGlobalUserVar[money;$sum[$getGlobalUserVar[money];300]]
   $globalCooldown[7d;{newEmbed:{title:heyo Slow Down}{description:you have to wait for at least about %time%}{color:BLURPLE}}]`
 }
