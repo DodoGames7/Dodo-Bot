689971ebe08f173c20ee00789956273721132943
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-10-14 11:29:32 +0200

    Done

---
+++ commands/economy/shop.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: "shop",
+    code: `$title[Shop]
+    $description[❓ **hecker card**
+    **description**: you could just use this to hack someone's bank imo
+    **item id**: \`heckercard\`
+    
+    
+    🌮 **taco**
+    **description**: eat some taco so you can become strong for 10 mins
+    **item id**: \`taco\`
+    
+    
+    💀 **RickRoller**
+    **description**: the most powerful item to rickroll your friends ever existed
+    **item id**: \`rickroller\`
+    ]
+    `
+    }
