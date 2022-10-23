54d4cc225457f8449699b5dd998963a4f3373ca8
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-10-23 22:07:46 +0200

    same thing

--- index.js
+++ index.js
@@ -22,15 +22,3 @@ require('./utils/callbacks')(bot) // for loading most callbacks used in bot
  "./commands/" is the path of folder where all the commands' code will be present
  */
 
-const  { Panel } = require("@akarui/aoi.panel")
-// v6 panel dashboard
-const panel = new Panel({
-    username: process.env.username,//username for logging in
-    password: process.env.password,//password for logging in
-    secret: process.env.secret,//session secret
-    port: 3000,//port on which website is hosted, Not required! Default 3000
-    bot: bot,//your aoi.js client
-    mainFile: "index.js",//Main file where code is running.Not required, default taken from package.json
-    commands: "./commands",// folder name in which all the edit needing files are there.
-    vesrion:"v6"
-})
