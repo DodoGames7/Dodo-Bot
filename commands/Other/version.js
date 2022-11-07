6d42a72feeebcf5f90a7b5433bd2055cf606cb8f
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-11-08 00:12:16 +0200

    fix trivia bug with author buttons

--- commands/Other/version.js
+++ commands/Other/version.js
@@ -1,24 +1,13 @@
 module.exports ={
   name:"version",
   aliases:["updates","news","ver"],
-  code: `$title[** ⚙️Version: 1.7.1 (2022 oct 14th) **(Beta)**]
+  code: `$title[** ⚙️Version: 1.7.1 (2022 nov 8th) **(Beta)**]
 $description[
-  an new beta build of 1.7.1 has been published!, here are the following new changes:
-  added profiles system!, you can now have an profile in economy with the ability to change embed color with both bio and rank to your liking!
-  added an shop with items system (including inventory), they're still WIP so it's very incomplete
-  improved slash commands such as \`\say\`, context menu commands may be added soon
-  finished chatbot system
-  added an settings to customize your profile in economy (\`settings\`)
-  
-  **other**
-  updated node.js version to 16.17.1
-  replaced the gift emoji in \`Remind me\` button with timer thingy
-  show time left in both \`daily\` and \`weekly\` command for better economy experience!
-
-  **deprecations**
-  deprecate \`f\` command and replace it with \`sus\` command, \`Reason\`: api used for \`f\` command got shutdown
+  **fixes**
+  fixed trivia set mode command being broken with author buttons only
 
 
 ]
-$color[BLURPLE]`
+$color[BLURPLE]
+$footer[Build: 9plbxGK]`
 } // returns a list of changes in the bot code
--- commands/utils/set-trivia-mode.js
+++ commands/utils/set-trivia-mode.js
@@ -3,19 +3,19 @@ module.exports = [{
   code: `
   $title[1;difficulty]
   $description[1; you can change the difficulty of trivia mode to other types by choosing the buttons below]
-  $addButton[1;Hard;primary;trivia_hard_mode_$authorID;no]
-  $addButton[1;Medium;primary;trivia_medium_mode_$authorID;no]
-  $addButton[1;Easy;primary;trivia_easy_mode_$authorID;no]
+  $addButton[1;Hard;primary;triviahard_$authorID;no]
+  $addButton[1;Medium;primary;triviamedium_$authorID;no]
+  $addButton[1;Easy;primary;triviaeasy_$authorID;no]
   $editIn[2m; {
  "embeds" : "{newEmbed:{title:difficulty}{description:you can change the difficulty of trivia mode to other types by choosing the buttons below}}" ,
- "components" : "{actionRow:{button:Easy:primary:trivia_easy_mode:yes}{button:Medium:primary:trivia_medium_mode:yes}{button:Hard:primary:trivia_hard_mode:yes}}"
+ "components" : "{actionRow:{button:Easy:primary:triviaeasy:yes}{button:Medium:primary:triviamedium:yes}{button:Hard:primary:triviahard:yes}}"
  }]
   $onlyPerms[manageserver;you need \`MANAGE_SERVER\` permission in order to use this]
  `
 },{
 type: "interaction",
 prototype: "button",
-code: `$interactionUpdate[;{newEmbed:{title:difficulty}{description:difficulty has been changed to \`easy\` mode now! $setServerVar[trivia_game_mode;easy]}};{actionRow:{button:Easy:success:trivia_easy_mode_$get[authorID]:yes:}{button:Medium:primary:trivia_medium_mode_$get[authorID]:yes:}{button:Hard:primary:trivia_hard_mode_$get[authorID]:yes:}
+code: `$interactionUpdate[;{newEmbed:{title:difficulty}{description:difficulty has been changed to \`easy\` mode now! $setServerVar[trivia_game_mode;easy]}};{actionRow:{button:Easy:success:triviaeasy_$get[authorID]:yes:}{button:Medium:primary:triviamedium_$get[authorID]:yes:}{button:Hard:primary:triviahard_$get[authorID]:yes:}
 ;;no}]
 
 $onlyif[$get[authorID]==$interactionData[author.id];{
@@ -24,7 +24,7 @@ $onlyif[$get[authorID]==$interactionData[author.id];{
   "options" : { "interaction" : true }
   }]
   
-  $onlyif[$get[customId]==trivia_easy_mode;]
+  $onlyif[$get[customId]==triviaeasy;]
   
   $let[authorID;$splitText[2]]
   $let[customId;$splitText[1]] 
@@ -34,7 +34,7 @@ $onlyif[$get[authorID]==$interactionData[author.id];{
 },{
   type: "interaction",
   prototype: "button",
-  code: `$interactionUpdate[;{newEmbed:{title:difficulty}{description:difficulty has been changed to \`medium\` mode now! $setServerVar[trivia_game_mode;medium]}};{actionRow:{button:Easy:primary:trivia_easy_mode_$get[authorID]:yes:}{button:Medium:success:trivia_medium_mode_$get[authorID]:yes:}{button:Hard:primary:trivia_hard_mode_$get[authorID]:yes:}
+  code: `$interactionUpdate[;{newEmbed:{title:difficulty}{description:difficulty has been changed to \`medium\` mode now! $setServerVar[trivia_game_mode;medium]}};{actionRow:{button:Easy:primary:triviaeasy_$get[authorID]:yes:}{button:Medium:success:triviamedium_$get[authorID]:yes:}{button:Hard:primary:triviahard_$get[authorID]:yes:}
 ;;no}]
 
 $onlyif[$get[authorID]==$interactionData[author.id];{
@@ -43,7 +43,7 @@ $onlyif[$get[authorID]==$interactionData[author.id];{
   "options" : { "interaction" : true }
   }]
   
-  $onlyif[$get[customId]==trivia_medium_mode;]
+  $onlyif[$get[customId]==triviamedium;]
   
   $let[authorID;$splitText[2]]
   $let[customId;$splitText[1]] 
@@ -51,7 +51,7 @@ $onlyif[$get[authorID]==$interactionData[author.id];{
 },{
   type: "interaction",
   prototype: "button",
-  code: `$interactionUpdate[;{newEmbed:{title:difficulty}{description:difficulty has been changed to \`hard\` mode now! $setServerVar[trivia_game_mode;hard]}};{actionRow:{button:Easy:primary:trivia_easy_mode_$get[authorID]:yes:}{button:Medium:primary:trivia_medium_mode_$get[authorID]:yes:}{button:Hard:success:trivia_hard_mode_$get[authorID]:yes:};;no}]
+  code: `$interactionUpdate[;{newEmbed:{title:difficulty}{description:difficulty has been changed to \`hard\` mode now! $setServerVar[trivia_game_mode;hard]}};{actionRow:{button:Easy:primary:triviaeasy_$get[authorID]:yes:}{button:Medium:primary:triviamedium_$get[authorID]:yes:}{button:Hard:success:triviahard_$get[authorID]:yes:};;no}]
   
   $onlyif[$get[authorID]==$interactionData[author.id];{
     "content" : "You aren't the author of this interaction.",
@@ -59,7 +59,7 @@ $onlyif[$get[authorID]==$interactionData[author.id];{
     "options" : { "interaction" : true }
     }]
     
-    $onlyif[$get[customId]==trivia_hard_mode;]
+    $onlyif[$get[customId]==triviahard;]
     
     $let[authorID;$splitText[2]]
     $let[customId;$splitText[1]] 
