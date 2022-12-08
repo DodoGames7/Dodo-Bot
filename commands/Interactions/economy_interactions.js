14bd24cea8f59f5fa944c8c88eb5d2e565abd332
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-12-08 17:58:16 +0200

    improve `$setTimeout` to use json data

--- commands/Interactions/economy_interactions.js
+++ commands/Interactions/economy_interactions.js
@@ -3,13 +3,17 @@ name: "daily",
 type: "interaction",
 prototype: "button",
 code: `$interactionReply[you will be reminded after one day of daily!;;;;everyone;yes]
-$setTimeout[remind;1d;user: $interactionData[author.id]] 
+$setTimeout[remind;1d;{
+  "user":"$interactionData[author.id]"
+}] 
 `
 },{
   name: "weekly",
   type: "interaction",
   prototype: "button",
   code: `$interactionReply[you will be reminded after one week!;;;;everyone;yes]
-$setTimeout[wremind;7d;user: $interactionData[author.id]]
+$setTimeout[wremind;7d;{
+  "user":"$interactionData[author.id]"
+}]
 `
 }]
