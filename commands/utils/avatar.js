f38ad85ed041dd63331af0d815b58cf6246450a8
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-12-22 16:19:43 +0200

    add a link to download avatar

--- commands/utils/avatar.js
+++ commands/utils/avatar.js
@@ -2,5 +2,6 @@ module.exports = {
   name: "avatar",
   aliases: ["av","userav"],
   code: `$title[1;$username[$mentioned[1;yes]]'s Avatar]
-  $image[1;$userAvatar[$mentioned[1;yes]] `
+  $image[1;$userAvatar[$mentioned[1;yes]]
+  $addButton[1;Download;link;$nonEscape[$userAvatar[$mentioned[1;yes]]];no]`
 }
