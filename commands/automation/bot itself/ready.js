fa6aeea274aa9672dc0bf4afa8cdeebe4a83f06a
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-04-21 19:50:37 +0000

    add warning

--- commands/automation/bot itself/ready.js
+++ commands/automation/bot itself/ready.js
@@ -2,5 +2,6 @@ module.exports = {
   type: "ready",
   channel: "",
   code: `$log[Dodo Bot is ready! Invite it using this link: $getBotInvite[sendmessages;viewchannel;readmessagehistory]
+  Note: please note that v2 isn't fully finished Yet as it has been started from scratch. You can still report bugs on Issues page.
 ]`
 }
