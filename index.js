d204e2c3d5321948351d835ceaf3cc503daccebc
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-04-21 20:09:35 +0000

    add comments + add note on `prefix`

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const { AoiClient, LoadCommands, Util} = require("aoi.js");
 
 const bot = new AoiClient({
-  token: process.env.TOKEN,
-  prefix: "$getGuildVar[prefix]",
-  intents: ["MessageContent", "Guilds", "GuildMessages", "DirectMessages"],
-  events: ["onMessage", "onInteractionCreate"],
-  aoiLogs: false,
-  aoiWarning: true,
-  guildOnly: false
+  token: process.env.TOKEN, // token is private by using env method
+  prefix: "$getGuildVar[prefix]", // By default, it uses custom prefix system.
+  intents: ["MessageContent", "Guilds", "GuildMessages", "DirectMessages"], // discord.js intents (v14)
+  events: ["onMessage", "onInteractionCreate"], // mostly for making the bot interactions work
+  aoiLogs: false, // don't show aoi.js default console message
+  aoiWarning: true, // disable aoi.js update warning
+  guildOnly: false // don't limit commands to server only
 });
 
 
