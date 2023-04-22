903b23fefc3e84865aed88f912aecd04855decd8
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-04-22 19:40:38 +0000

    add links to invite cmd

--- commands/main/invite.js
+++ commands/main/invite.js
@@ -2,7 +2,12 @@ module.exports = {
   name: "invite",
   executeAt: "both",
   code: `$title[invite Dodo Bot]
-  $description[Dodo Bot is an entertainment bot trying it's best to make users have fun while at the same time, provide useful cmds to the user.]
+  $description[Dodo Bot is an entertainment bot trying it's best to make users have fun while at the same time, provide useful cmds to the user.
+
+**Links you might want to check it out**
+[Dodo-Bot is no longer multi-purpose](https://github.com/DodoGames7/Dodo-Bot/tree/v2#its-no-longer-multi-purpose)
+[Askers](https://discord.ly/askers)
+]
   $addButton[1;Official server;5;https://discord.gg/pFwKjAaZvj;false]
   $addButton[1;Invite;5;$nonEscape[$getBotInvite[sendmessages;readmessagehistory;viewchannel]];false]
   $cooldown[2s;slow down, don't spam the command]
