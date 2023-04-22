55e781858068ee8edba2836fa7a967c8416774e9
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-04-22 12:11:00 +0000

    uppercase M

--- commands/main/settings.js
+++ commands/main/settings.js
@@ -4,7 +4,7 @@ module.exports = {
   aliases: "panel",
   code: `$title[Advanced Settings]
   $description[This settings let's you manage any feature or settings of an certain system, for example managing on how trivia should start.]
-  $addSelectMenu[1;settingsmenu_$authorID;select an Option;1;1;false;Games:manage settings related to games.:gamesmenu:false;general:manage settings related to general things.:generalmenu:false]
+  $addSelectMenu[1;settingsmenu_$authorID;select an Option;1;1;false;Games:Manage settings related to games.:gamesmenu:false;general:Manage settings related to general things.:generalmenu:false]
   $cooldown[2s;slow down, don't spam the command]
   $onlyPerms[managemessages; you need \`ManageMessages\` permission to access this panel.]`
 }
