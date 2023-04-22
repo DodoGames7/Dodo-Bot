0f41f43336f0d285fb446a1df712e0e6ef6d8aec
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-04-22 19:37:13 +0000

    use comma

--- commands/Entertainment/games/coinflip.js
+++ commands/Entertainment/games/coinflip.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "coinflip",
   executeAt: "both",
   code: `$title[Coin Flip]
-  $description[The coin is in the air Choose heads or tails below.]
+  $description[The coin is in the air, choose heads or tails below.]
   $color[Red]
   $addButton[1;Tails;1;tailsbutton_$authorID;false]
   $addButton[1;Heads;4;headsbutton_$authorID;false]
