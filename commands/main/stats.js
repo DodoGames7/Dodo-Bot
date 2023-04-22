e6d3accfdf3eeae5cf95c9f9c1dc80a01b7a2c9e
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-04-22 15:15:43 +0000

    make ram look better

--- commands/main/stats.js
+++ commands/main/stats.js
@@ -9,7 +9,7 @@ $addField[Aoi.js version;\`$packageVersion\`;true]
 $addField[Uptime Bot;$uptime[humanize];true]
 $addField[Ping;$pingms;true]
 $addField[Cpu Usage;$cpu;true]
-$addField[Ram Usage;$ram;true]
+$addField[Ram Usage;$round[$ram]%;true]
 $addField[Users;$allMembersCount;true]
 $cooldown[2s;slow down, don't spam the command]
 `
