7c911b73d4e8d73474747c6ce8ea12d62f188d0b
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-04-23 14:34:19 +0000

    Improve `serverinfo` cmd

--- commands/Util/serverinfo.js
+++ commands/Util/serverinfo.js
@@ -3,14 +3,33 @@ module.exports = {
   executeAt: "guild",
   aliases: "server",
   code: `$title[Server Info]
-  $addField[verification level;$get[verifylevel]]
-  $addField[Roles Count;$roleCount]
-  $addField[Emojis Count;$emojiCount[$guildID]]
-  $addField[Channels Count;$channelCount]
-  $addField[Members Count;$membersCount]
+  $addField[Verification Level;$get[verifylevel]]
+  $addField[Other;
+server Created since: $creationDate[$guildID;date]
+Content Filter: $get[content]
+Server ID: $guildID
+]
+  $addField[General;
+Roles: $roleCount
+Emojis: $emojiCount[$guildID] 
+Server Boosts: $guildBoostCount[$guildID]
+
+]
+  $addField[Channels;
+Total: $channelCount[$guildID;all]
+Text Channels: $channelCount[$guildID;Text]
+Voice Channels: $channelCount[$guildID;Voice]
+Categorys: $channelCount[$guildID;Category]
+]
+  $addField[Members information;
+Total: $membersCount
+Humans: $membersCount[$guildID;all;false]
+Bots: $botCount[$guildID]
+]
   $thumbnail[$userAvatar[$clientID]]
   $cooldown[2s;slow down, don't spam the command]
   
   $let[verifylevel;$replaceText[$replaceText[$replaceText[$replaceText[$GuildVerificationLevel;1;Low];2;Medium];3;High];4;Very High]]
+  $let[content;$replaceText[$replaceText[$replaceText[$guildContentFilter[$guildID];0;Disabled];1;Medium];2;High]]
 `
 }
