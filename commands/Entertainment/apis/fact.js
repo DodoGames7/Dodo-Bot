1a920762c73c6a224189d6f06aedb6ef6196d4fd
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-04-24 15:11:36 +0000

    use cooldown on `fact` + `info` cmd

--- commands/Entertainment/apis/fact.js
+++ commands/Entertainment/apis/fact.js
@@ -2,5 +2,7 @@ module.exports = {
   name: "fact",
   executeAt: "both",
   code: `$sendMessage[$nonEscape[$getobjectproperty[fact]];false]
-  $createobject[$jsonRequest[https://api.popcat.xyz/fact]]`
+  $createobject[$jsonRequest[https://api.popcat.xyz/fact]]
+  $cooldown[5s;slow down, don't spam the command]
+  `
 }
---
+++ commands/main/info.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "info",
+    code: `$title[important Information regarding v2]
+    $description[Be aware that v2 is currently in beta, Features, and other stuff like uis of current Commands are subject to change.]
+    $color[Blurple]
+    $cooldown[5s;slow down, don't spam the command]
+    `
+    }
