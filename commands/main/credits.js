36f9ff7db082c57aedbfd09b22548eac729eaa01
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-05-19 02:00:45 +0300

    don't use tags to credit anymore

--- commands/main/credits.js
+++ commands/main/credits.js
@@ -3,11 +3,11 @@ module.exports = {
     code: `$title[Credits]
     $description[
 **Credits for v2**
-    $userTag[632607624742961153] - developer of the bot
+    $username[632607624742961153] - developer of the bot
 
 **Credits for v1 (discontinued Release)**
-$userTag[632607624742961153] - developer of the bot
-$userTag[760580796695969844] - previous help in the bot
+$username[632607624742961153] - developer of the bot
+$username[760580796695969844] - previous help in the bot
 ]
 $color[Blurple]
 `
