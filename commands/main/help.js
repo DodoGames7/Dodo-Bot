83ad2ed91d99cf0c8c46784dcbf5801a43edf2b6
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-05-19 20:05:26 +0300

    fixed footer not being closed

--- commands/main/help.js
+++ commands/main/help.js
@@ -3,8 +3,8 @@ module.exports = {
   executeAt: "both",
   code: `$title[Help Menu]
   $description[Select one of the modules to view it's available commands!]
-  $footer[developed by $userTag[632607624742961153]
+  $footer[developed by $username[632607624742961153]]
   $color[Blurple]
-  $addSelectMenu[1;helpmenu_$authorID;Select an Module;1;1;false;Main:explore General Commands:general:false;Entertainment:explore fun commands:entertainment:false;Util:explore util commands:util:false;info:explore information commands:info:false]
+  $addSelectMenu[1;helpmenu_$authorID;Select an Module;1;1;false;Main:explore General Commands:general:false;Entertainment:explore fun commands:entertainment:false;Util:explore util commands:util:false]
   $cooldown[2s;slow down, don't spam the command]`
 }
