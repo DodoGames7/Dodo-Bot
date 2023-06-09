9ac4a31f1085edb73bff90d06183c9474f179ae6
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-06-09 12:11:11 +0300

    updated 6.3.0 to latest + update credits

--- commands/main/credits.js
+++ commands/main/credits.js
@@ -4,6 +4,7 @@ module.exports = {
     $description[
 **Credits for v2**
     $username[632607624742961153] - developer of the bot
+    $username[431749535656837130] - inspiration to improve some commands
 
 **Credits for v1 (discontinued Release)**
 $username[632607624742961153] - developer of the bot
