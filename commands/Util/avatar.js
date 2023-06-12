81c714226a4b6e0aed304007cc4cd258fe784ce9
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-06-12 10:08:13 +0300

    View avatar for those not in a server

--- commands/Util/avatar.js
+++ commands/Util/avatar.js
@@ -2,9 +2,10 @@ module.exports = {
   name: "avatar",
   executeAt: "guild",
   aliases: ["av","userav"],
-  code: `$title[$username[$mentioned[1;true]]'s Avatar]
-  $image[$userAvatar[$mentioned[1;true]]
-  $addButton[1;Download;5;$nonEscape[$userAvatar[$mentioned[1;true]]];false]
-  $cooldown[2s;slow down, don't spam the command]
+  code: `$title[$username[$findUser[$message;true]]'s Avatar]
+  $image[$userAvatar[$findUser[$message;true]]
+  $addButton[1;Download;5;$nonEscape[$userAvatar[$findUser[$message;true]]];false]
+  $cooldown[5s; Slow down! Don't spam the command!
+Time left: \`%time%\`]
   `
 }
