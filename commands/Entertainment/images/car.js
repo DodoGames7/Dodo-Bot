f08bcf3e76d4ac111ed1a49951f836836830d9fb
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-06-12 11:14:43 +0300

    better error handling in `car`

--- commands/Entertainment/images/car.js
+++ commands/Entertainment/images/car.js
@@ -2,8 +2,9 @@ module.exports = {
     name: "car",
     code: `
     $image[$getObjectProperty[image]]
+    $onlyIf[$getObjectProperty[image]!=undefined;Something went wrong while searching for an image, please try again.]
     $createObject[$jsonRequest[https://api.popcat.xyz/car]]
-    $cooldown[5s; slow down, don't spam the command]
-    $suppressErrors[Something went wrong while searching for an image, please try again.]
+    $cooldown[5s; Slow down! Don't spam the command!
+    Time left: \`%time%\`]
                                `
     }
