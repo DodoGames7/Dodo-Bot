637a2e7b386d8349c97923117f1d99f6077c5081
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-06-17 09:44:39 +0300

    support new usernames

--- commands/Util/avatar.js
+++ commands/Util/avatar.js
@@ -2,9 +2,10 @@ module.exports = {
   name: "avatar",
   executeAt: "guild",
   aliases: ["av","userav"],
-  code: `$title[$username[$findUser[$message;true]]'s Avatar]
+  code: `$title[$get[usernamechecker]'s Avatar]
   $image[$userAvatar[$findUser[$message;true]]
   $addButton[1;Download;5;$nonEscape[$userAvatar[$findUser[$message;true]]];false]
+  $let[usernamechecker;$replaceText[$replaceText[$checkCondition[$charCount[$discriminator[$findUser[$message;true]]]==1];true;$username[$findUser[$message;true]]];false;$userTag[$findUser[$message;true]]]]
   $cooldown[5s; Slow down! Don't spam the command!
 Time left: \`%time%\`]
   `
--- commands/automation/Interactions/helpmenu.js
+++ commands/automation/Interactions/helpmenu.js
@@ -19,7 +19,7 @@ $onlyIf[$interactionData[values[0]]==general;]`
 },{
   type: "interaction",
   prototype: "selectMenu",
-  code: `$interactionUpdate[;{newEmbed:{title:Entertainment}{field:Games:\`ctf\`, \`rps\`, \`snake\`, \`matchpairs\`, \`2048\`, \`flood\` \`minesweeper\`, \`slots\`, \`trivia\`, \`c4\`, \`findemoji\`, \`coinflip\`, \`akinator\` }{field:General Fun stuff:\`say\`, \`howgamer\`, \`fact\`}{field:Images:\`car\`}};{actionRow:{selectMenu:helpmenu_$get[authorID]:Select an Module:1:1:false:{selectMenuOptions:Main:general:explore General Commands:false}{selectMenuOptions:Entertainment:entertainment:explore Fun Commands:false}{selectMenuOptions:Util:util:explore Util Commands:false}}}]
+  code: `$interactionUpdate[;{newEmbed:{title:Entertainment}{field:Games:\`ctf\`, \`rps\`, \`snake\`, \`matchpairs\`, \`2048\`, \`flood\` \`minesweeper\`, \`slots\`, \`trivia\`, \`c4\`, \`findemoji\`, \`coinflip\`, \`akinator\` }{field:General Fun stuff:\`say\`, \`howgamer\`, \`fact\`, \`whoasked\`}{field:Images:\`car\`}};{actionRow:{selectMenu:helpmenu_$get[authorID]:Select an Module:1:1:false:{selectMenuOptions:Main:general:explore General Commands:false}{selectMenuOptions:Entertainment:entertainment:explore Fun Commands:false}{selectMenuOptions:Util:util:explore Util Commands:false}}}]
 
 
   
@@ -38,7 +38,7 @@ $onlyIf[$interactionData[values[0]]==entertainment;]
 },{
   type: "interaction",
   prototype: "selectMenu",
-  code: `$interactionUpdate[;{newEmbed:{title:Util}{field:Managing:\`prefix\`,\`reset-prefix\`, \`suggest-set\`, \`suggest-reset\`}{field:Info:\`serverinfo\`}{field:other Util:\`avatar\`, \`suggest\`}{description:**Note**#COLON# \`prefix\` allows you to also change prefix, just make sure your command has arguments on it such as numbers or anything else you like!}};{actionRow:{selectMenu:helpmenu_$get[authorID]:Select an Module:1:1:false:{selectMenuOptions:Main:general:explore General Commands:false}{selectMenuOptions:Entertainment:entertainment:explore Fun Commands:false}{selectMenuOptions:Util:util:explore Util Commands:false}}}]
+  code: `$interactionUpdate[;{newEmbed:{title:Util}{field:Managing:\`prefix\`,\`reset-prefix\`, \`suggest-set\`, \`suggest-reset\`}{field:Info:\`serverinfo\`, \`user\`}{field:other Util:\`avatar\`, \`suggest\`, \`randomhex\`}{description:**Note**#COLON# \`prefix\` allows you to also change prefix, just make sure your command has arguments on it such as numbers or anything else you like!}};{actionRow:{selectMenu:helpmenu_$get[authorID]:Select an Module:1:1:false:{selectMenuOptions:Main:general:explore General Commands:false}{selectMenuOptions:Entertainment:entertainment:explore Fun Commands:false}{selectMenuOptions:Util:util:explore Util Commands:false}}}]
 
 
   
