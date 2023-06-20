8d751e9cda6e0085f18c48c4a422abde556d4e95
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-06-20 21:59:22 +0300

    updated `user` again

--- commands/Util/user.js
+++ commands/Util/user.js
@@ -2,7 +2,8 @@ module.exports = {
     name: "user",
     $if: "old",
     aliases: ["userinfo", "user-info"],
-    code: `$author[User information for $username[$findUser[$message;true]];$userAvatar[$clientID]]
+    code: `
+    $author[User information for $get[usernamechecker];$userAvatar[$clientID]]
     $addField[Other;
     User banner color (if any): $replaceText[$replaceText[$checkCondition[$userBannerColor[$findUser[$message;true]]!=];false;No color has been found];true;$userBannerColor[$findUser[$message;true]]]
     [Avatar Link]($userAvatar[$findUser[$message;true]])
@@ -19,12 +20,14 @@ module.exports = {
     $addField[Server information;
     Joined the server at: <t:$truncate[$divide[$memberJoinDate[$findUser[$message;true];$guildID];1000]]:f>
     Highest Role: $highestRole[$findUser[$message;true];$guildID;name]
+    Lowest Role: $roleName[$lowestRole[$findUser[$message;true];$guildID]]
     Status: $userStatus[$guildID;$findUser[$message;true]]
     Server Booster: $isBoosting[$findUser[$message;true];$guildID]
     ]
     $endif
+    $let[usernamechecker;$replaceText[$replaceText[$checkCondition[$charCount[$discriminator[$findUser[$message;true]]]==1];true;$username[$findUser[$message;true]]];false;$userTag[$findUser[$message;true]]]]
     $cooldown[5s; Slow down! Don't spam the command!
-Time left: \`%time%\`]
+    Time left: \`%time%\`]
     `
     
     }
