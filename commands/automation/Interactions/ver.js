4f42137808bf70751b22129b64131da0c408d1a6
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-08-07 08:34:16 +0300

    move that to bug fixes category

--- commands/automation/Interactions/ver.js
+++ commands/automation/Interactions/ver.js
@@ -9,7 +9,6 @@ module.exports = [{
 * Removed \`readfile\` command from the bot
 * Updated returned invite links in commands such as \`invite\` to work with the latest stuff of discord
 * Moved test message buttons of both leave and welcome to their settings
-* Returning the bot's token using dev commands is no longer possible to increase security
     }{color:Blurple}};{actionRow:{button:Changes:1:versionchanges_$authorID:true}{button:Bug Fixes:3:versionbugfixes_$authorID:false}{button:Other:2:versionother_$authorID:true}};;all;true]
     
     
@@ -34,6 +33,7 @@ module.exports = [{
 * Group ids for dms no longer breaks channel detections in commands such as \`suggest-set\` command!
 * Fixed markdown formatting of this command
 * Fixed mentions being usable in the beta command \`reverse\`
+* Returning the bot's token using dev commands is no longer possible to increase security
     }{color:Blurple}};{actionRow:{button:Changes:1:versionchanges_$authorID:false}{button:Bug Fixes:3:versionbugfixes_$authorID:true}{button:Other:2:versionother_$authorID:true}};;all;true]
     
     
