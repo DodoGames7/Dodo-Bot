8ce4f128754e55456a678a97508952f07ae47e57
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-08-28 00:07:51 +0300

    simplyify ban message

--- commands/automation/Interactions/others/ban-logs.js
+++ commands/automation/Interactions/others/ban-logs.js
@@ -1,18 +1,12 @@
 module.exports = {
 name: "ban-logger",
 type: "banAdd",
-$if: "old",
 channel: "$getGuildVar[banneduserschannel]",
 code: `
-This is currently in beta stage so not much information is shown.
 $author[$username was banned from the server!;$userAvatar;$userAvatar]
-$if[$memberExists[$authorID]==true]
-$addField[Information;
+$description[**Information**
 Account age: $creationDate[$authorID;date]
 ]
-$else
-$description[It appears that they were banned while not being a member in this server... No member information will be shown then.]
-$endif
 $footer[Banned $get[botchecker] ID: $authorID]
 $addTimeStamp
 $color[Red]
