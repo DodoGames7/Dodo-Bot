c0409ee5d0d7addcdaeb85c0f979e97948dafd14
DodoGames7 <69091682+DodoGames7@users.noreply.github.com>
2023-11-18 14:42:53 +0200

    bruh

--- commands/automation/Interactions/ver.js
+++ commands/automation/Interactions/ver.js
@@ -23,7 +23,7 @@ module.exports = [{
 * Fixed a strange issue where permission check system is broken in select menu channels thus being able to select channels the bot didn't have access to (this will re-add the same error back, won't actually touch the "menu")
 * Fixed the version simply still reading as "2.0.0" in \`stats\` command.
 * Fixed inconsistencies between \`welcomer\` and \`leave\` command at "Getting started" part
-* Fixed the flag \`--embed\` not working from previous build of 2.0.1 on Gthub
+* Fixed the flag \`--embed\` not working from previous build of 2.0.1 on Github
 * Fixed view channel permissions being not enforced on features like message delete logging
     }{color:Blurple}};{actionRow:{button:Changes:1:versionchanges_$authorID:false}{button:Bug Fixes:3:versionbugfixes_$authorID:true}{button:Other:2:versionother_$authorID:false}{button:Full Changelog:5:https#COLON#//github.com/DodoGames7/Dodo-Bot/releases:false}};;all;true]
     
