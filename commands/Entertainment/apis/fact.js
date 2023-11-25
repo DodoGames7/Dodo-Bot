6fce299094110f6cfe2b247cdbb344cf65cc9ae2
DodoGames7 <69091682+DodoGames7@users.noreply.github.com>
2023-11-25 10:13:41 +0200

    Fixed `fact` being dead

--- commands/Entertainment/apis/fact.js
+++ commands/Entertainment/apis/fact.js
@@ -1,8 +1,8 @@
 module.exports = {
   name: "fact",
   executeAt: "both",
-  code: `$sendMessage[$nonEscape[$getobjectproperty[fact]];false]
-  $createobject[$jsonRequest[https://api.popcat.xyz/fact]]
+  code: `$sendMessage[$nonEscape[$getobjectproperty[api;fact]];false]
+  $createobject[api;$jsonRequest[https://api.popcat.xyz/fact]]
   $cooldown[5s; Slow down! Don't spam the command!
 Time left: \`%time%\`]`
 }
