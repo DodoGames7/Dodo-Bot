3ae714b31b192b2297f5366c4699563f084e6d7c
DodoGames7 <69091682+DodoGames7@users.noreply.github.com>
2023-11-28 04:16:35 +0200

    Fixed `car`
    `car` has been dead for a while. This update should fix it.

--- commands/Entertainment/images/car.js
+++ commands/Entertainment/images/car.js
@@ -1,9 +1,9 @@
 module.exports = {
 name: "car",
 code: `
-$image[$getObjectProperty[image]]
-$onlyIf[$getObjectProperty[image]!=undefined;Something went wrong while searching for an image, please try again.]
-$createObject[$jsonRequest[https://api.popcat.xyz/car]]
+$image[$getObjectProperty[api;image]]
+$onlyIf[$getObjectProperty[api;image]!=undefined;Something went wrong while searching for an image, please try again.]
+$createObject[api;$jsonRequest[https://api.popcat.xyz/car]]
 $suppressErrors[{newEmbed:{title:Uh.. Oh!}{description:Something went wrong while running the command. If you still get this message then you may try again later if possible.
 
 If waiting didn't help and you still get this message then it's most likely that the stuff we use for car command are down for now.}{color:Red}}]
