b17ff6647089f1bc1e4a495e64b419800ba2f398
DodoGames7 <69091682+DodoGames7@users.noreply.github.com>
2023-11-29 07:35:32 +0200

    Updated node deprecation's msg

--- commands/automation/bot itself/node-deprecation.js
+++ commands/automation/bot itself/node-deprecation.js
@@ -3,7 +3,7 @@ module.exports = {
     type: "ready",
     channel: "",
     code: `
-    $log[It seems that you're using node.js $nodeVersion. Be mindful that support for those versions are deprecated as they have lost support and so in the future, packages used in Dodo-Bot may stop supporting it at anytime. Use these versions at your own risk.]
+    $log[It seems that you're using Node.js $nodeVersion. Be mindful that support for those versions are deprecated as they have lost support and so in the future, packages used in Dodo-Bot may stop supporting it anytime soon. Use these versions at your own risk.]
     
     $onlyIf[$stringStartsWith[$nodeVersion;v16;v17]==true;]`
     }
