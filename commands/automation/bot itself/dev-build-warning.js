830ebebf01b931444e77ef355e3455975bff55af
DodoGames7 <69091682+DodoGames7@users.noreply.github.com>
2023-12-05 04:14:52 +0200

    Force beta to display dev warn

--- commands/automation/bot itself/dev-build-warning.js
+++ commands/automation/bot itself/dev-build-warning.js
@@ -4,5 +4,5 @@ module.exports = {
     channel: "",
     code: `
     $log[Development version detected! Dev builds can be unstable despite having the latest bug fixes! By using them, you agree that you're using it at your own risk. It is HIGHLY recommended to use stable version for now until the upcoming version becomes stable enough.]
-    $onlyIf[$stringEndswith[$getVar[botversion];-dev]==true;]`
+    $onlyIf[$stringEndswith[$getVar[botversion];-dev;-beta]==true;]`
 }
