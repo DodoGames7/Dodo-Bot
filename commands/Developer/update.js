6ee5d73d132a1c7ee8f230b8753c62a9b3b2d406
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2024-03-28 13:57:14 +0200

    bruh?

--- commands/Developer/update.js
+++ commands/Developer/update.js
@@ -5,7 +5,7 @@ code: `Successfully updated all commands.
 $addButton[1;Total: $commandsCount;2;uselesscustomid;true]
 $updateCommands
 $cooldown[5s;Slow down! Don't spam the command!
-Time remaining: <t:$truncate[$divide[$sum[$getCooldownTime[5s;user;update;$authorID];$dateStamp];1000]]:R>]]
+Time remaining: <t:$truncate[$divide[$sum[$getCooldownTime[5s;user;update;$authorID];$dateStamp];1000]]:R>]
 $onlyIf[$checkContains[$clientOwnerIDs[,];$authorID]==true;]
 `
 }
