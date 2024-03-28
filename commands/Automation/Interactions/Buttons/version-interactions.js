f3ae2a52a124c0b03511bd77502d8a588bea12c2
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2024-03-28 13:44:14 +0200

    Fix spacing

--- commands/Automation/Interactions/Buttons/version-interactions.js
+++ commands/Automation/Interactions/Buttons/version-interactions.js
@@ -17,9 +17,9 @@ module.exports = [{
     prototype: "button",
     code: `
     $interactionUpdate[{newEmbed:{title:Bug Fixes}{description:
-      * Fixed coinflip buttons being not disabled properly
-      * Fixed developer panel's emojis on options disappearing after pressing on a random button then \`Home\` button
-      * Fixed message logging delete having a random space before the displayed deleted content is shown
+* Fixed coinflip buttons being not disabled properly
+* Fixed developer panel's emojis on options disappearing after pressing on a random button then \`Home\` button
+* Fixed message logging delete having a random space before the displayed deleted content is shown
     }{color:$getVar[embedcolor]}}{actionRow:{button:Home:2:homebutton_$authorID:false:🏠}{button:Changes:2:versionchanges_$authorID:false}{button:Bug Fixes:2:versionbugfixes_$authorID:true}{button:Other:2:versionother_$authorID:false}}{actionRow:{button:Version history:5:https#COLON#//github.com/DodoGames7/Dodo-Bot/releases:false:🔎}}]
     
     $onlyIf[$advancedTextSplit[$interactionData[customId];_;2]==$interactionData[author.id];{newEmbed:{title:Uh, Oh!}{description:You're not the author of this interaction.}{color:Red}}
