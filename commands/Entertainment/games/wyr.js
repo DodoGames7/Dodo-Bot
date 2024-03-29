2ce8c1be74c5b17048ea166299b2e7be45a9f5a0
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2024-03-29 21:06:27 +0200

    New error message for wyr

--- commands/Entertainment/games/wyr.js
+++ commands/Entertainment/games/wyr.js
@@ -8,10 +8,7 @@ $addButton[1;0;2;wyr2-votebutton;false;2️⃣]
 $addButton[1;0;2;wyr1-votebutton;false;1️⃣]
 $color[$getVar[embedcolor]]
 $createObject[api;$jsonRequest[https://api.popcat.xyz/wyr]]
-$suppressErrors[{newEmbed:{title:Uh.. Oh!}{description:Something went wrong while running the command. If you still get this message then you may try again later if possible.
-
-If waiting didn't help and you still get this message then it's most likely that the stuff we use for wyr command are down for now.}{color:Red}}]
-
+$suppressErrors[Unable to fetch data for wyr. Please, try again later.]
 $cooldown[5s; Slow down! Don't spam the command!
 Time remaining: <t:$truncate[$divide[$sum[$getCooldownTime[5s;user;would-you-rather;$authorID];$dateStamp];1000]]:R>]
 `
