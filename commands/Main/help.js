c7e80beb310afa7f0112f3fa6bb3fbe24fc69158
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2024-03-30 13:32:47 +0200

    Leveling module for help cmd (wip)

--- commands/Main/help.js
+++ commands/Main/help.js
@@ -4,7 +4,7 @@ module.exports = {
   $description[To view commands, select one of the modules!]
   $footer[Made with ❤️ by $username[632607624742961153]]
   $color[$getVar[embedcolor]]
-$addSelectMenu[1;string;helpmenu_$authorID;Select an Module;1;1;false;Main:Explore commands from Main module:general:false;Entertainment:Explore commands from Entertainment module:entertainment:false;Util:Explore commands from Util module:util:false]
+  $addSelectMenu[1;string;helpmenu_$authorID;Select an Module;1;1;false;Main:Explore commands from Main module:general:false;Entertainment:Explore commands from Entertainment module:entertainment:false;Leveling:Explore commands from Leveling module:leveling:false;Util:Explore commands from Util module:util:false]
 $cooldown[2s; Slow down! Don't spam the command!
 Time remaining: <t:$truncate[$divide[$sum[$getCooldownTime[2s;user;help;$authorID];$dateStamp];1000]]:R>]`
 }
