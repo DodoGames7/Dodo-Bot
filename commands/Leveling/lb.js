a828d8228fcf8816c891893568245fb4e40ece97
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2024-03-31 20:21:40 +0200

    Add the missing cooldown to lb

--- commands/Leveling/lb.js
+++ commands/Leveling/lb.js
@@ -7,5 +7,7 @@ module.exports ={
 $let[leaderboard;$advancedReplaceText[$userLeaderBoard[$guildID;level;desc;{top} - {username} - Level {value};10;1;main];1 -;🥇 1 -;2 -;🥈 2 -;3 -;🥉 3 -]]
     $color[1;$getVar[embedcolor]]
     $onlyIf[$getGuildVar[levelsystem]==on;Leveling is not enabled currently.]
+    $cooldown[5s;Slow down! Don't spam the command!
+Time remaining: <t:$truncate[$divide[$sum[$getCooldownTime[5s;user;level-leaderboard;$authorID];$dateStamp];1000]]:R>]
 `
 }
