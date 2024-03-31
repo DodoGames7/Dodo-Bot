78ac7f0f543d739db861ccfd557e46671d48fbbe
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2024-03-31 19:46:17 +0200

    Medals support

--- commands/Leveling/lb.js
+++ commands/Leveling/lb.js
@@ -3,7 +3,8 @@ module.exports ={
     aliases: ["lb", "leveling-lb", "leaderboard"],
     code: `
     $title[$guildName's Leaderboard]
-    $description[1;$userLeaderBoard[$guildID;level;desc;{top} - {username} - Level {value};10;1;main]]   
+    $description[$get[leaderboard]]
+$let[leaderboard;$advancedReplaceText[$userLeaderBoard[$guildID;level;desc;{top} - {username} - Level {value};10;1;main];1 -;🥇 1.;2 -;🥈 2.;3 -;🥉 3.]]
     $color[1;$getVar[embedcolor]]
     $onlyIf[$getGuildVar[levelsystem]==on;Leveling is not enabled currently.]
 `
