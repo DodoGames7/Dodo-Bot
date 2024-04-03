8c356b9fd0e34a2ca8ace2e826a0fee7f6aa4e87
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2024-04-03 21:16:24 +0200

    More stable leaderboard

--- commands/Leveling/lb.js
+++ commands/Leveling/lb.js
@@ -4,7 +4,7 @@ module.exports ={
     code: `
     $title[$guildName's Leaderboard]
     $description[$get[leaderboard]]
-$let[leaderboard;$advancedReplaceText[$userLeaderBoard[$guildID;level;desc;{top} - {username} - Level {value};10;1;main];1 -;🥇 1 -;2 -;🥈 2 -;3 -;🥉 3 -]]
+$let[leaderboard;$advancedReplaceText[$userLeaderBoard[$guildID;level;desc;&{top} - {username} - Level {value};10;1;main];&1 -;🥇 1 -;&2 -;🥈 2 -;&3 -;🥉 3 -;&;]]
     $color[1;$getVar[embedcolor]]
     $onlyIf[$getGuildVar[levelsystem]==on;Leveling is not enabled currently.]
     $cooldown[5s;Slow down! Don't spam the command!
