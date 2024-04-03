03e303ea806b90bc33b0473b3a03f68db6a032eb
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2024-04-04 00:07:56 +0200

    Allow 0 percent for HowGamer

--- commands/Entertainment/general/howgamer.js
+++ commands/Entertainment/general/howgamer.js
@@ -1,7 +1,7 @@
 module.exports = {
 name: "howgamer",
 code: `$title[HowGamer machine]
-$description[$get[target] $random[1;100]% Gamer]
+$description[$get[target] $random[0;100]% Gamer]
 $color[Random]
 $let[target;$replaceText[$replaceText[$checkCondition[$mentioned[1;true]==$authorID];true;You're];false;$username[$mentioned[1;true]] is]]
 $cooldown[2s; Slow down! Don't spam the command!
