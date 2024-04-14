19bb0592df5e1d05398e6e1d1c64e04d0bf21e32
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2024-04-14 23:12:05 +0200

    Errors should now work for `car` cmd

--- commands/Entertainment/images/car.js
+++ commands/Entertainment/images/car.js
@@ -6,8 +6,8 @@ $title[Here's a random car]
 $thumbnail[$getObjectProperty[api;image]]
 $addField[About this car;$getObjectProperty[api;title]]
 $addButton[1;Image link;5;$nonEscape[$getObjectProperty[api;image]];false]
-$onlyIf[$IsValidImageLink[$getObjectProperty[api;image]]==true;It looks like there're issues with processing the image. Please, try again later if possible.]
 $createObject[api;$jsonRequest[https://api.popcat.xyz/car]]
+$suppressErrors[It looks like there're issues with processing the image. Please, try again later if possible.]
 $cooldown[5s; Slow down! Don't spam the command!
 Time remaining: <t:$truncate[$divide[$sum[$getCooldownTime[5s;user;car;$authorID];$dateStamp];1000]]:R>]
                            `
