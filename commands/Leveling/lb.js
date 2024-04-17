2c4c381605cb256c33f1531d22646fc72cdc3d8e
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2024-04-17 03:48:57 +0200

    Level lb: don't show the server name

--- commands/Leveling/lb.js
+++ commands/Leveling/lb.js
@@ -2,7 +2,7 @@ module.exports ={
     name: "level-leaderboard",
     aliases: ["lb", "leveling-lb", "leaderboard"],
     code: `
-    $title[$guildName's Leaderboard]
+    $title[Leveling Leaderboard]
     $description[$get[leaderboard]]
 $let[leaderboard;$advancedReplaceText[$userLeaderBoard[$guildID;level;desc;&{top} - {username} - Level {value};10;1;main];&1 -;🥇 1 -;&2 -;🥈 2 -;&3 -;🥉 3 -;&;]]
     $color[1;$getVar[embedcolor]]
