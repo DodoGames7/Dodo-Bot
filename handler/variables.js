851224fbae86302267017fc23ba249d2ab03030a
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2024-04-18 06:13:30 +0200

    Update changelog

--- handler/variables.js
+++ handler/variables.js
@@ -13,7 +13,7 @@ module.exports = {
   autopingreply: "off",
   includebots: "on",
   version: "2.1.0",
-  buildDate: "1713409595521",
+  buildDate: "1713413587752",
   buildType: "Pre-Beta 2",
   buildRevision: "0",
   embedcolor: "#2A3759",
