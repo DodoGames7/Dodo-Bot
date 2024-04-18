efd6465795cda0d45fab6d6b8dedadb3579daf72
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2024-04-18 07:18:34 +0200

    Typo

--- commands/Automation/Interactions/Buttons/version-interactions.js
+++ commands/Automation/Interactions/Buttons/version-interactions.js
@@ -5,7 +5,7 @@ module.exports = [{
     $interactionUpdate[{newEmbed:{title:Changes}{description:
 * Added a new game titled \`Guess the Pokemon\` under the names, \`guess-the-pokemon\` and \`gtp\`
 * Add a check whether or not the current build of the bot is a GitBuilds build in build info button
-* A re-work Welcomer settings has been introduced. It's counterpart Leave will eventually receive the same treatment at one point
+* A re-worked Welcomer settings has been introduced. It's counterpart Leave will eventually receive the same treatment at one point
     }{color:$getVar[embedcolor]}}{actionRow:{button:Home:2:homebutton_$authorID:false:🏠}{button:Changes:2:versionchanges_$authorID:true}{button:Bug Fixes:2:versionbugfixes_$authorID:false}{button:Other:2:versionother_$authorID:false}}{actionRow:{button:Version history:5:https#COLON#//github.com/DodoGames7/Dodo-Bot/releases:false:🔎}}]
     
     $onlyIf[$advancedTextSplit[$interactionData[customId];_;2]==$interactionData[author.id];{newEmbed:{title:Uh, Oh!}{description:You're not the author of this interaction.}{color:Red}}
