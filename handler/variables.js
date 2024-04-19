b249527f61459d2d120b1834ff173930fbd79677
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2024-04-19 23:22:31 +0200

    Backport missing comma fix here

--- handler/variables.js
+++ handler/variables.js
@@ -13,7 +13,7 @@ module.exports = {
   autopingreply: "off",
   includebots: "on",
   version: "2.1.0",
-  buildDate: "1713464018823",
+  buildDate: "1713561740117",
   buildType: "Post-Refresh",
   buildRevision: "3",
   embedcolor: "#2A3759",
