34859b13b7d578a523befdd24f17e8de1070023d
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2024-05-18 14:07:45 +0300

    Fixed ban reason

--- commands/Automation/Events/ban-logging.js
+++ commands/Automation/Events/ban-logging.js
@@ -13,7 +13,7 @@ module.exports = [{
     $addTimeStamp
     $color[Red]
     $let[moderatorchecker;$replaceText[$replaceText[$checkCondition[$isBot==false&&$getGuildVar[anonymous]==off];true;$username[$get[staff]] <@$get[staff]>];false;Unknown]]
-    $let[reason;$replaceText[$replaceText[$checkCondition[$getAuditLogs[$guildID;;1;22;{reason}]==null;true;None.];false;$getAuditLogs[$guildID;;1;22;{reason}]]]
+    $let[reason;$replaceText[$replaceText[$checkCondition[$getAuditLogs[$guildID;;1;22;{reason}]==null];true;None.];false;$getAuditLogs[$guildID;;1;22;{reason}]]]
     $let[staff;$getAuditLogs[$guildID;;1;22;{executor.id}]]
     $let[botchecker;$replaceText[$replaceText[$checkCondition[$isBot==true];true;bot];false;user]]
     $onlyIf[$hasPermsInChannel[$getGuildVar[banneduserschannel];$clientID;viewchannel;sendmessages]==true;]
