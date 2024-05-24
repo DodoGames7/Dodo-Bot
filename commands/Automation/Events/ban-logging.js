0bfcfa948630011debd29bcee82273edf47d7f66
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2024-05-24 09:41:40 +0300

    Make unban logs show reason and moderator

--- commands/Automation/Events/ban-logging.js
+++ commands/Automation/Events/ban-logging.js
@@ -16,26 +16,32 @@ module.exports = [{
     $let[reason;$replaceText[$replaceText[$checkCondition[$getAuditLogs[$guildID;;1;22;{reason}]==null];true;None.];false;$getAuditLogs[$guildID;;1;22;{reason}]]]
     $let[staff;$getAuditLogs[$guildID;;1;22;{executor.id}]]
     $let[botchecker;$replaceText[$replaceText[$checkCondition[$isBot==true];true;bot];false;user]]
-    $onlyIf[$hasPermsInChannel[$getGuildVar[banneduserschannel];$clientID;viewchannel;sendmessages]==true;]
     $onlyIf[$hasPerms[$guildID;$clientID;viewauditlog]==true;In order to have ban logs work, i must have \`ViewAuditLog\` permission!]
+    $onlyIf[$hasPermsInChannel[$getGuildVar[banneduserschannel];$clientID;viewchannel;sendmessages]==true;]
     $onlyIf[$guildChannelExists[$guildID;$getGuildVar[banneduserschannel]]==true;]
     $onlyIf[$getGuildVar[banneduserschannel]!=none;]
     $onlyIf[$guildID==$guildID;]
     `
     },{
-    name: "Un-Ban Logs",
-    type: "banRemove",
-    channel: "$getGuildVar[unbanneduserschannel]",
-    code: `$author[Member unbanned!;$userAvatar;$userAvatar]
-$description[$username was unbanned from the server! They can now join the server again.]
-$footer[Unbanned $get[botchecker] ID: $authorID]
-$addTimeStamp
-$color[Green]
-$let[botchecker;$replaceText[$replaceText[$checkCondition[$isBot==true];true;bot];false;member]]
-$onlyIf[$hasPermsInChannel[$getGuildVar[unbanneduserschannel];$clientID;viewchannel;sendmessages]==true;]
-$onlyIf[$guildChannelExists[$guildID;$getGuildVar[unbanneduserschannel]]==true;]
-$onlyIf[$getGuildVar[unbanneduserschannel]!=none;]
-$onlyIf[$guildID==$guildID;]
-
-`
-}]
+        name: "Un-Ban Logs",
+        type: "banRemove",
+        channel: "$getGuildVar[unbanneduserschannel]",
+        code: `$author[Member unbanned!;$userAvatar;$userAvatar]
+    $description[
+    **$toLocaleUpperCase[$get[botchecker]]:** $username <@$authorID>
+    **Moderator:** $get[moderator]
+    ]
+    $footer[ID: $authorID]
+    $addTimeStamp
+    $color[DarkGreen]
+    $let[moderator;$username[$get[staff]] <@$get[staff]>]
+    $let[staff;$getAuditLogs[$guildID;;1;22;{executor.id}]]
+    $let[botchecker;$replaceText[$replaceText[$checkCondition[$isBot==true];true;bot];false;member]]
+    $onlyIf[$hasPerms[$guildID;$clientID;viewauditlog]==true;In order to have ban logs work, i must have \`ViewAuditLog\` permission!]
+    $onlyIf[$hasPermsInChannel[$getGuildVar[unbanneduserschannel];$clientID;viewchannel;sendmessages]==true;]
+    $onlyIf[$guildChannelExists[$guildID;$getGuildVar[unbanneduserschannel]]==true;]
+    $onlyIf[$getGuildVar[unbanneduserschannel]!=none;]
+    $onlyIf[$guildID==$guildID;]
+    
+    `
+    }]
