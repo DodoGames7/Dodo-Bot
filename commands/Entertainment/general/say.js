d6e95a9fbff175d46c51e4eba67fee4577a8fc8f
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2024-05-24 09:35:00 +0300

    Fixed outdated perms in say cmd

--- commands/Entertainment/general/say.js
+++ commands/Entertainment/general/say.js
@@ -16,7 +16,7 @@ $endif
 $onlyIf[$message[1]!=;Say something first.
 **Note**: To use the embed mode, pass the flag \`--embed\` first before you start with the content of the message.
 $disableMentionType[all]
-$let[botlink;$nonEscape[$getClientInvite[sendmessages;viewchannel;addreactions;attachfiles]]]
+$let[botlink;$nonEscape[$getClientInvite[sendmessages;viewchannel;addreactions;attachfiles;viewauditlog]]]
 $let[usernamechecker;$replaceText[$replaceText[$checkCondition[$charCount[$discriminator[$authorID]]==1];true;$username];false;$userTag]]
  $cooldown[5s; Slow down! Don't spam the command!
 Time remaining: <t:$truncate[$divide[$sum[$getCooldownTime[5s;user;say;$authorID];$dateStamp];1000]]:R>]
