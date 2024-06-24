bf6aa31038ec93120cf99ce0e959aa2c3ec3d3dd
dodoGames <dodogames@localhost.localdomain>
2024-06-25 02:51:52 +0300

    implement channel check correctly

--- commands/Automation/Others/Bot specific/startupchannel.js
+++ commands/Automation/Others/Bot specific/startupchannel.js
@@ -8,7 +8,7 @@ module.exports = {
     $useChannel[$getVar[startupchannel]]
     
     $onlyIf[$hasPermsInChannel[$getVar[startupchannel];$clientID;sendmessages;viewchannel]==true;]
-    $onlyIf[$guildChannelExists[$guildID;$getVar[startupchannel]]==true;]
+    $onlyIf[$channelExists[$getVar[startupchannel]]==true;]
     $onlyIf[$getVar[startupchannelsystem]==on;]
     $onlyIf[$getVar[startupchannel]!=none;]`
     }
