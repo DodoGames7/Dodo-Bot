a1df933ff4f6dbf9c4d0060df51be8d8186a4bc1
dodoGames <dodogames@localhost.localdomain>
2024-06-26 15:36:05 +0300

    Changed comments in index

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { AoiClient } = require("aoi.js");
-const config = require("./config.json");
-require('dotenv').config()
+const config = require("./config.json"); // Load the setup options from config
+require('dotenv').config() // Enable env support in local hosting
 
 // Client Setup
 const client = new AoiClient({
   token: process.env.TOKEN || config.BotToken, // Token with either env or config
-  prefix: "$getGuildVar[prefix]", // By default, it uses custom prefix system. (default used prefix: d!)
+  prefix: "$getGuildVar[prefix]", // By default, it uses custom prefix system. (default prefix used: d!)
   intents: ["MessageContent", "Guilds", "GuildMessages", "GuildMembers", "GuildPresences", "GuildModeration", "GuildEmojisAndStickers"], // Discord.js intents (v14)
   events: ["onMessage", "onInteractionCreate", "onJoin", "onMessageDelete", "onMessageUpdate", "onLeave", "onBanAdd", "onBanRemove", "onGuildJoin", "onFunctionError"], // Setup aoi.js events
   aoiLogs: false, // Don't show aoi.js default console message
@@ -15,12 +15,12 @@ const client = new AoiClient({
     db: require("@akarui/aoi.db"),
     dbType: "KeyValue",
     tables: ["main"],
-    securityKey: process.env.DBsecurityKey || config.DBsecurityKey
+    securityKey: process.env.DBsecurityKey || config.DBsecurityKey // Security Key with either env or config
   },
  disableFunctions: ["$clientToken"], // For safety reasons
  mobilePlatform: config.MobileStatus, // Whether or not to enable mobile status
- debugs: { // Whether or not to enable aoi.js debug mode
- interpreter: config.DebugClient
+ debugs: {
+ interpreter: config.DebugClient // Whether or not to enable aoi.js debug mode
 }
 });
 
