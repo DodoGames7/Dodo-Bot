970028065d3e55fed8e52b276fd4eafcb8a192d5
dodoGames <dodogames@localhost.localdomain>
2024-06-27 02:19:55 +0300

    I fucked smth

--- commands/Util/ban-log-commands.js
+++ commands/Util/ban-log-commands.js
@@ -5,7 +5,7 @@ info: {
     perms: ["`SendMessages`", "`ManageChannels`"],
     usage: "set-banlog channel-name/channel ID or <#channel ID>"
 },
-aliases: "set-banlogs"
+aliases: "set-banlogs",
 code: `$setGuildVar[banneduserschannel;$findChannel[$message]]
 Successfully set <#$findChannel[$message]> as the Ban logs channel!
 $onlyIf[$hasPermsInChannel[$findChannel[$message];$clientID;sendmessages;viewchannel]==true;Hmm. Seems like i don't have permissions there, please ensure that i have the following for this channel:
@@ -27,7 +27,7 @@ info: {
     description: "Reset current channel used for Ban logs.",
     perms: ["`SendMessages`", "`ManageChannels`"]
 },
-aliases: "reset-banlogs"
+aliases: "reset-banlogs",
 code: `$deleteVar[banneduserschannel;$guildID;main]
 Successfully reset the ban log channel!
 $onlyIf[$getGuildVar[banneduserschannel]!=none;There is no channel to reset currently.]
@@ -42,7 +42,7 @@ $onlyPerms[managechannels;You do not have \`ManageChannels\` permission to use t
         perms: ["`SendMessages`", "`ManageChannels`"],
         usage: "set-unbanlog channel-name/channel ID or <#channel ID>",
     },
-    aliases: "set-unbanlogs"
+    aliases: "set-unbanlogs",
     code: `$setGuildVar[unbanneduserschannel;$findChannel[$message]]
     Successfully set <#$findChannel[$message]> as the un-ban logs channel!
     $onlyIf[$hasPermsInChannel[$findChannel[$message];$clientID;sendmessages;viewchannel]==true;Hmm. Seems like i don't have permissions there, please ensure that i have the following for this channel:
@@ -63,7 +63,7 @@ Time remaining: <t:$truncate[$divide[$sum[$getCooldownTime[5s;user;set-unbanlog;
         description: "Reset current channel used for un-ban logs.",
         perms: ["`SendMessages`", "`ManageChannels`"]
     },
-    aliases: "reset-unbanlogs"
+    aliases: "reset-unbanlogs",
     code: `$deleteVar[unbanneduserschannel;$guildID;main]
     Successfully reset the un-ban log channel!
     $onlyIf[$getGuildVar[unbanneduserschannel]!=none;There is no channel to reset currently.]
