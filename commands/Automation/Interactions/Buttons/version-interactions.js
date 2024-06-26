82cc551023cd94bb9cffac6ab475f9fa4a6b101f
dodoGames <dodogames@localhost.localdomain>
2024-06-27 02:06:25 +0300

    Here we go!

--- commands/Automation/Interactions/Buttons/version-interactions.js
+++ commands/Automation/Interactions/Buttons/version-interactions.js
@@ -4,7 +4,7 @@ module.exports = [{
     code: `
     $interactionUpdate[{newEmbed:{title:Changes}{description:
 * \`whoasked\` command now edits every 5 seconds rather than 6 secs
-* The newer luck system has been applied to \`coinflip\` command
+* Added aliases for ban-logs commands
 * The invite link to support server has been removed from \`invite\` command
     }{color:$getVar[embedcolor]}$nonEscape[$get[devbuild]]}{actionRow:{button:Home:2:homebutton_$authorID:false:🏠}{button:Changes:2:versionchanges_$authorID:true}{button:Bug Fixes:2:versionbugfixes_$authorID:false}{button:Other:2:versionother_$authorID:false}}{actionRow:{button:Version history:5:https#COLON#//github.com/DodoGames7/Dodo-Bot/releases:false:🔎}}]
 
@@ -37,6 +37,7 @@ $let[devbuild;$if[$getVar[pre_release_mode]==on;{footer:Testing is recommended:h
     prototype: "button",
     code: `$interactionUpdate[{newEmbed:{title:Other}{description:
 * Changed the branch of build to \`main\`
+* The newer luck system has been applied to \`coinflip\` command
 * Wording has been slightly changed in "Use of source code" from \`credits\`
 * (Source code) The text for invite on console has been colored blue (and separated from main text as well)
  }{color:$getVar[embedcolor]}$nonEscape[$get[devbuild]]}{actionRow:{button:Home:2:homebutton_$authorID:false:🏠}{button:Changes:2:versionchanges_$authorID:false}{button:Bug Fixes:2:versionbugfixes_$authorID:false}{button:Other:2:versionother_$authorID:true}}{actionRow:{button:Version history:5:https#COLON#//github.com/DodoGames7/Dodo-Bot/releases:false:🔎}}]
