ee7c102ca94fda95b3d7ffa437dc9d3db0b8ccdb
dodoGames <dodogames@localhost.localdomain>
2024-07-26 19:32:24 +0300

    Fixed `whereiseveryone` cmd

--- commands/Entertainment/Images/whereiseveryone.js
+++ commands/Entertainment/Images/whereiseveryone.js
@@ -8,20 +8,20 @@ info: {
 code: `
 $userCooldown[whereiseveryonecmd;2s;Cooldown has been triggered! Please, wait!
 Time remaining: <t:$trunc[$divide[$sum[$getTimestamp;$getUserCooldownTime[whereiseveryonecmd]];1000]]:R>]
-$attachment[./handler/assets/whereiseveryone-part1.png;shrek1.png]
-$attachment[./handler/assets/whereiseveryone-part2.jpg;shrek2.png]
-$attachment[./handler/assets/whereiseveryone-part3.jpg;shrek3.png]
 
 
 $let[messageID;$sendMessage[$channelID;$title["It's quiet..."]
+$attachment[./handler/assets/whereiseveryone-part1.png;shrek1.png]
 $image[attachment://shrek1.png]
 ;true]]
 $wait[5000]
 $!editMessage[$channelID;$get[messageID];$title["Too quiet..."]
+$attachment[./handler/assets/whereiseveryone-part2.jpg;shrek2.png]
 $image[attachment://shrek2.png]
 ]
 $wait[5000]
 $!editMessage[$channelID;$get[messageID];$title["Where is Everyone?"]
+$attachment[./handler/assets/whereiseveryone-part3.jpg;shrek3.png]
 $image[attachment://shrek3.png]
 ]
 
