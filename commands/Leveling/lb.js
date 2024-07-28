e1114ec25de487d656a40f901f6a81290061c24c
dodoGames <dodogames@localhost.localdomain>
2024-07-28 08:38:58 +0300

    Block number zero

--- commands/Leveling/lb.js
+++ commands/Leveling/lb.js
@@ -14,6 +14,7 @@ module.exports ={
 * Members do not have level 2 or higher. Please wait for someone to level up first then try again.
 * The specified Leaderboard page still didn't register members. Wait for a couple of members to level up then try again
     ]
+$onlyIf[$get[page]>=1;You can't go less than page 1.]
 $onlyIf[$get[page]<=10;You can only switch up to page 10.]
     $onlyIf[$isNumber[$get[page]]==true;A invalid page has been entered. Please specify a existing page.]
 $let[leaderboard;$advancedReplaceText[$userLeaderBoard[$guildID;level;desc;&{top} - {username} - Level {value};10;$get[page];main];&1 -;🥇 1 -;&2 -;🥈 2 -;&3 -;🥉 3 -;&;]]
