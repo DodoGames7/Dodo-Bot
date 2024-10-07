0f9dc78e90cd5a65dfb6e9c9bb2bfb0c8ccbcbd8
dodoGames <dodogames@localhost.localdomain>
2024-10-07 12:37:32 +0300

    last minute

--- commands/Automation/Interactions/Buttons/version-interactions.js
+++ commands/Automation/Interactions/Buttons/version-interactions.js
@@ -21,7 +21,7 @@ $let[devbuild;$if[$getVar[pre_release_mode]==on;{footer:Testing is recommended:h
     prototype: "button",
     code: `
     $interactionUpdate[{newEmbed:{title:Bug Fixes}{description:
-*No bug fixes has been added yet*
+* Internal fix for a private command has been added
     }{color:$getVar[embedcolor]}$nonEscape[$get[devbuild]]}{actionRow:{button:Home:2:homebutton_$authorID:false:🏠}{button:Changes:2:versionchanges_$authorID:false}{button:Bug Fixes:2:versionbugfixes_$authorID:true}{button:Other:2:versionother_$authorID:false}}{actionRow:{button:Version history:5:https#COLON#//github.com/DodoGames7/Dodo-Bot/releases:false:🔎}}]
 
 $let[devbuild;$if[$getVar[pre_release_mode]==on;{footer:Testing is recommended:https#COLON#//us-east-1.tixte.net/uploads/dodogames.wants.solutions/warningsign.png};  ]]
