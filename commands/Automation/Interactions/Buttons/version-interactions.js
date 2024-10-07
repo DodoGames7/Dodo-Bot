dfcb98c6ed955042bbf088d5e2365f6a57bbac52
dodoGames <dodogames@localhost.localdomain>
2024-10-07 12:40:42 +0300

    also this

--- commands/Automation/Interactions/Buttons/version-interactions.js
+++ commands/Automation/Interactions/Buttons/version-interactions.js
@@ -37,6 +37,7 @@ $let[devbuild;$if[$getVar[pre_release_mode]==on;{footer:Testing is recommended:h
     prototype: "button",
     code: `$interactionUpdate[{newEmbed:{title:Other}{description:
 * (Source Code) extended variable handler code to support tables
+* (Source Code) Special functions for aoi has been finally enabled after being first added (left unused) in 2.1.5
 * Added a new module for \`help\` dedicated to bot owners only
 * Updated main credits for \`credits\` (they're no longer tied to discord usernames anymore)
  }{color:$getVar[embedcolor]}$nonEscape[$get[devbuild]]}{actionRow:{button:Home:2:homebutton_$authorID:false:🏠}{button:Changes:2:versionchanges_$authorID:false}{button:Bug Fixes:2:versionbugfixes_$authorID:false}{button:Other:2:versionother_$authorID:true}}{actionRow:{button:Version history:5:https#COLON#//github.com/DodoGames7/Dodo-Bot/releases:false:🔎}}]
