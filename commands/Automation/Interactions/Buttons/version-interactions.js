dfd99d5acce33afae396201d5fed16d8bca262e1
dodoGames <dodogames@localhost.localdomain>
2024-10-10 19:29:37 +0300

    Bug fix

--- commands/Automation/Interactions/Buttons/version-interactions.js
+++ commands/Automation/Interactions/Buttons/version-interactions.js
@@ -21,7 +21,7 @@ $let[devbuild;$if[$getVar[pre_release_mode]==on;{footer:Testing is recommended:h
     prototype: "button",
     code: `
     $interactionUpdate[{newEmbed:{title:Bug Fixes}{description:
-*No bug fixes has been added yet*
+* Fixed \`version\` command being unusable (Revision 1)
     }{color:$getVar[embedcolor]}$nonEscape[$get[devbuild]]}{actionRow:{button:Home:2:homebutton_$authorID:false:🏠}{button:Changes:2:versionchanges_$authorID:false}{button:Bug Fixes:2:versionbugfixes_$authorID:true}{button:Other:2:versionother_$authorID:false}}{actionRow:{button:Changelog history:5:https#COLON#//github.com/DodoGames7/Dodo-Bot/releases:false:📜}}]
 
 $let[devbuild;$if[$getVar[pre_release_mode]==on;{footer:Testing is recommended:https#COLON#//us-east-1.tixte.net/uploads/dodogames.wants.solutions/warningsign.png};  ]]
@@ -55,11 +55,12 @@ $let[devbuild;$if[$getVar[pre_release_mode]==on;{footer:Testing is recommended:h
       type: "interaction",
     prototype: "button",
     code: `$interactionUpdate[{newEmbed:{title:Dodo-Bot version}{description:
-* **Version**#COLON# $getVar[version]$if[$getVar[buildRevision]!=0; (Revision $getVar[buildRevision])]
+* **Version**#COLON# $getVar[version]$get[Revision]
 * **Release type**#COLON# $getVar[release_type]
 * **$get[releasedatetype]**#COLON# <t:$truncate[$divide[$getVar[buildDate];1000]]:f>
  }{color:$getVar[embedcolor]}$nonEscape[$get[devbuild]]}{actionRow:{button:Changes:2:versionchanges_$authorID:false}{button:Bug Fixes:2:versionbugfixes_$authorID:false}{button:Other:2:versionother_$authorID:false}}{actionRow:{button:Changelog history:5:https#COLON#//github.com/DodoGames7/Dodo-Bot/releases:false:📜}}]
 
+ $let[Revision;$advancedReplaceText[$checkCondition[$getVar[buildRevision]!=0];true; (Revision $getVar[buildRevision]);false; ]]
  $let[releasedatetype;$advancedReplaceText[$checkCondition[$getVar[pre_release_mode]==on];true;Build created on;false;Released on]]
  $let[devbuild;$if[$getVar[pre_release_mode]==on;{footer:Testing is recommended:https#COLON#//us-east-1.tixte.net/uploads/dodogames.wants.solutions/warningsign.png};  ]]
 
--- commands/Main/ver.js
+++ commands/Main/ver.js
@@ -8,7 +8,7 @@ module.exports = {
   $if: "old",
   code: `$title[Dodo-Bot version]
   $description[
-* **Version**: $getVar[version]$if[$getVar[buildRevision]!=0; (Revision $getVar[buildRevision])]
+* **Version**: $getVar[version]$get[Revision]
 * **Release type**: $getVar[release_type]
 * **$get[releasedatetype]**: <t:$truncate[$divide[$getVar[buildDate];1000]]:f>
   ]
@@ -20,6 +20,7 @@ module.exports = {
   $addButton[1;Other;2;versionother_$authorID;false]
   $addButton[1;Bug Fixes;2;versionbugfixes_$authorID;false]
   $addButton[1;Changes;2;versionchanges_$authorID;false]
+  $let[Revision;$advancedReplaceText[$checkCondition[$getVar[buildRevision]!=0];true; (Revision $getVar[buildRevision]);false; ]]
   $let[releasedatetype;$advancedReplaceText[$checkCondition[$getVar[pre_release_mode]==on];true;Build created on;false;Released on]]
   $cooldown[2s; Slow down! Don't spam the command!
 Time remaining: <t:$truncate[$divide[$sum[$getCooldownTime[2s;user;version;$authorID];$dateStamp];1000]]:R>]
